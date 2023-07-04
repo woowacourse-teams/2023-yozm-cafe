0843d39a62e0e9e96502316a5c0456340b6a8a5c
solo5star <solo_5@naver.com>
2023-07-04 17:44:15 +0900

    chore: 프론트엔드 보일러플레이트 셋업

---
+++ frontend/webpack.config.js
@@ -0,0 +1,36 @@
+import CopyPlugin from 'copy-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+/** @type {import('webpack').Configuration} */
+export default {
+  mode: 'development',
+  entry: './src/index',
+  output: {
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[tj]sx?/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      filename: 'index.html',
+    }),
+    new CopyPlugin({
+      patterns: [{ from: 'public', to: '' }],
+    }),
+  ],
+  devServer: {
+    hot: true,
+    port: 3000,
+  },
+};
