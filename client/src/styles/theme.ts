33ddefbfd0f91e540ad39087e9642191b0e521a1
solo5star <solo_5@naver.com>
2023-07-05 04:37:17 +0000

    feat: 스타일 추가

---
+++ client/src/styles/theme.ts
@@ -0,0 +1,28 @@
+const theme = {
+  color: {
+    primary: '#f67952',
+    secondary: '#ffa559',
+  },
+  fontSize: {
+    xs: '0.75rem',
+    sm: '0.875rem',
+    base: '1rem',
+    lg: '1.125rem',
+    xl: '1.25rem',
+    '2xl': '1.5rem',
+    '3xl': '1.875rem',
+  },
+  space: {
+    '0.5': '0.125rem',
+    '1': '0.25rem',
+    '1.5': '0.375rem',
+    '2': '0.5rem',
+    '2.5': '0.625rem',
+    '3': '0.75rem',
+    '3.5': '0.875rem',
+    '4': '1rem',
+    '5': '1.25rem',
+  },
+} as const;
+
+export default theme;
