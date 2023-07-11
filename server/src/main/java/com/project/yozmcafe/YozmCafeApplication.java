c84e4fab030cff26531b5d536246aa167c06f2b8
donghae-kim <donghae1999@gmail.com>
2023-07-11 16:31:22 +0900

    feat: 백엔드 초기 설정
    #1

---
+++ server/src/main/java/com/project/yozmcafe/YozmCafeApplication.java
@@ -0,0 +1,13 @@
+package com.project.yozmcafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YozmCafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YozmCafeApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/project/yozmcafe/YozmCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.yozmcafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YozmCafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
