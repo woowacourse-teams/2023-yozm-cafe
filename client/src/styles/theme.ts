a1bde751b0dbc4a6b5a72b2d25ff83eaa6fe728b
solo5star <solo_5@naver.com>
2023-07-11 07:59:04 +0000

    feat: 디자인 시스템 수정

--- client/src/styles/theme.ts
+++ client/src/styles/theme.ts
@@ -1,7 +1,18 @@
 const theme = {
   color: {
-    primary: '#f67952',
-    secondary: '#ffa559',
+    primary: '#F08080',
+    secondary: '#FFD3D8',
+    error: '#F14646',
+    warning: '#EF8523',
+    success: '#31A115',
+    text: {
+      primary: '#212121',
+      secondary: '#787878',
+    },
+    line: {
+      primary: '#777777',
+      secondary: '#BABABA',
+    },
   },
   fontSize: {
     xs: '0.75rem',
@@ -22,6 +33,13 @@ const theme = {
     '3.5': '0.875rem',
     '4': '1rem',
     '5': '1.25rem',
+  },
+  shadow: {
+    '1': '0 0 2px 2px rgba(0, 0, 0, 0.2)',
+    '2': '0 0 4px 4px rgba(0, 0, 0, 0.2)',
+    '3': '0 0 6px 6px rgba(0, 0, 0, 0.2)',
+    '4': '0 0 8px 8px rgba(0, 0, 0, 0.2)',
+    '5': '0 0 10px 10px rgba(0, 0, 0, 0.2)',
   },
 } as const;
 
