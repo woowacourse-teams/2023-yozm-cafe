b421f75dd3355485353a404e7f5c86c4922a4ba4
solo5star <solo_5@naver.com>
2023-07-13 01:31:53 +0000

    chore: react-query 셋업

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,19 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import React from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import router from './router';
 import theme from './styles/theme';
 
+const queryClient = new QueryClient();
+
 const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 };
 
