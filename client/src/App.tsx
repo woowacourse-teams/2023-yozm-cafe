b6937e4fec08583050e41b803c4cd692ddf7f862
solo5star <solo_5@naver.com>
2023-07-13 08:01:46 +0000

    refactor: css 파일을 `createGlobalStyle` 로 변경

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,9 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import React from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import router from './router';
+import GlobalStyle from './styles/GlobalStyle';
+import ResetStyle from './styles/ResetStyle';
 import theme from './styles/theme';
 
 const queryClient = new QueryClient();
@@ -11,6 +12,8 @@ const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
+        <ResetStyle />
+        <GlobalStyle />
         <RouterProvider router={router} />
       </ThemeProvider>
     </QueryClientProvider>
