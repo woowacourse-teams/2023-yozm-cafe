2fd34cb72368da520381d6751cdf7173ec78304d
jeongwusi <jeongwusi@gmail.com>
2023-07-15 15:36:31 +0900

    feat: 좋아요 추가와 취소에 대한 handlers 추가

--- client/src/mocks/handlers.ts
+++ client/src/mocks/handlers.ts
@@ -6,4 +6,30 @@ export const handlers = [
   rest.get('/cards', (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(cafes));
   }),
+
+  // 좋아요 추가
+  rest.post('/cafes/:cafe_id/likes', (req, res, ctx) => {
+    // eslint-disable-next-line camelcase
+    const { cafe_id } = req.params;
+    const updatedCafes = cafes.map((cafe) => {
+      if (cafe.id === Number(cafe_id)) {
+        return { ...cafe, likes: cafe.likeCount + 1 };
+      }
+      return cafe;
+    });
+    return res(ctx.status(200), ctx.json(updatedCafes));
+  }),
+
+  // 좋아요 취소
+  rest.delete('/cafes/:cafe_id/likes', (req, res, ctx) => {
+    // eslint-disable-next-line camelcase
+    const { cafe_id } = req.params;
+    const updatedCafes = cafes.map((cafe) => {
+      if (cafe.id === Number(cafe_id) && cafe.likeCount > 0) {
+        return { ...cafe, likes: cafe.likeCount - 1 };
+      }
+      return cafe;
+    });
+    return res(ctx.status(200), ctx.json(updatedCafes));
+  }),
 ];
