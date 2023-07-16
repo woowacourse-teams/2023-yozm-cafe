d09e7260af45a56ee8dcfcf26cfc343dba3c7554
jeongwusi <jeongwusi@gmail.com>
2023-07-16 14:45:11 +0900

    feat: 공통으로 사용할 수 있는 Button 컴포넌트 생성

---
+++ client/src/components/Button.tsx
@@ -0,0 +1,28 @@
+import React, { ButtonHTMLAttributes } from 'react';
+import { styled } from 'styled-components';
+import theme from '../styles/theme';
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  width: string;
+  height: string;
+  border: string;
+  fontWeight: string;
+  color: keyof typeof theme.color;
+};
+
+const Button = ({ children, ...rest }: ButtonProps) => {
+  return <StyledButton {...rest}>{children}</StyledButton>;
+};
+export default React.memo(Button);
+
+const StyledButton = styled.button<ButtonProps>`
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+
+  font-weight: ${(props) => props.fontWeight};
+  color: ${({ theme }) => theme.color.text.primary};
+
+  background-color: ${(props) => theme.color[props.color]};
+  border: ${(props) => props.border};
+  border-radius: 40px;
+`;
