d31682f89c7249adbca8bd7de8cd885f61aec0f5
jeongwusi <jeongwusi@gmail.com>
2023-07-16 14:43:46 +0900

    feat: 프로젝트에 주아체 추가

--- client/src/styles/GlobalStyle.ts
+++ client/src/styles/GlobalStyle.ts
@@ -3,6 +3,13 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyle = createGlobalStyle`
   @import url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css');
 
+  @font-face {
+    font-family: 'BMJUA';
+    font-weight: normal;
+    font-style: normal;
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_one@1.0/BMJUA.woff') format('woff');
+  }
+
   * {
     font-family: 'Pretendard', sans-serif;
   }
