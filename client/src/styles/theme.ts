8f441a7529246eb4c7bf45ddd1b36143572b901c
AhnGeunTaek <dksrmsxor@gmail.com>
2023-07-16 18:28:37 +0900

    chore: 스토리북 styled components 설정

--- client/src/styles/theme.ts
+++ client/src/styles/theme.ts
@@ -61,6 +61,9 @@ const theme = {
     '4': '0 0 8px 8px rgba(0, 0, 0, 0.2)',
     '5': '0 0 10px 10px rgba(0, 0, 0, 0.2)',
   },
+
+  lightTheme: {},
+  darkTheme: {},
 } as const;
 
 export default theme;
