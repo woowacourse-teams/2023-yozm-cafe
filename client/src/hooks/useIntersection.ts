d3a17a3bfce89ff60f9b415e2a32237188fd95df
solo5star <solo_5@naver.com>
2023-07-17 13:54:52 +0900

    feat: `useIntersection` 커스텀 훅 구현

---
+++ client/src/hooks/useIntersection.ts
@@ -0,0 +1,43 @@
+import { RefObject, useEffect, useState } from 'react';
+
+/**
+ * 주어진 ref의 HTMLElement가 화면 상에
+ * 교차되는지 여부를 observe하고 그 결과 값을 반환하는 훅입니다.
+ *
+ * @example
+ * const Example = () => {
+ *   const ref = useRef<HTMLDivElement>(null);
+ *   const intersection = useIntersection(ref, { threshold: 1 });
+ *
+ *   return (
+ *     <div ref={ref}>
+ *       {intersection?.intersectionRatio === 1
+ *         ? '전체가 보이는 중'
+ *         : '잘 안보이는 중'}
+ *     </div>
+ *   );
+ * }
+ */
+const useIntersection = (ref: RefObject<HTMLElement>, options: IntersectionObserverInit = {}) => {
+  const [entry, setEntry] = useState<IntersectionObserverEntry | null>(null);
+
+  useEffect(() => {
+    if (ref.current) {
+      const handler = (entries: IntersectionObserverEntry[]) => {
+        setEntry(entries[0]);
+      };
+
+      const observer = new IntersectionObserver(handler, options);
+      observer.observe(ref.current);
+
+      return () => {
+        setEntry(null);
+        observer.disconnect();
+      };
+    }
+  }, [ref.current, options.threshold, options.root, options.rootMargin]);
+
+  return entry;
+};
+
+export default useIntersection;
