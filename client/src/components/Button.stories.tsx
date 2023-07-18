f68064f73f655068677c7220c19eb3f12f49c9a5
jeongwusi <jeongwusi@gmail.com>
2023-07-18 11:20:47 +0900

    feat: Button 컴포넌트 추가

---
+++ client/src/components/Button.stories.tsx
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Button from './Button';
+
+type Story = StoryObj<typeof Button>;
+
+const meta: Meta<typeof Button> = {
+  title: 'Button',
+  component: Button,
+};
+
+export default meta;
+
+export const Default: Story = {
+  args: {},
+};
---
+++ client/src/components/Button.tsx
@@ -0,0 +1,46 @@
+import { ButtonHTMLAttributes } from 'react';
+import styled from 'styled-components';
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  variant?: 'contained' | 'outlined';
+  $fullWidth?: boolean;
+};
+
+const Button = ({ children, variant = 'contained', $fullWidth = false, ...rest }: ButtonProps) => {
+  return (
+    <Container variant={variant} $fullWidth={$fullWidth} {...rest}>
+      {children}
+    </Container>
+  );
+};
+
+export default Button;
+
+const Container = styled.button<ButtonProps>`
+  cursor: pointer;
+
+  padding: ${({ theme }) => theme.space['1.5']} 0;
+
+  font-size: 16px;
+  font-weight: 500;
+
+  border-radius: 40px;
+
+  ${(props) => {
+    if (props.variant === 'contained') {
+      return `
+        background-color: ${props.theme.color.primary};
+        color: ${props.theme.color.white};
+        border: none;
+      `;
+    } else if (props.variant === 'outlined') {
+      return `
+        background-color: ${props.theme.color.white};
+        color: ${props.theme.color.text.secondary};
+        border: 2px solid ${props.theme.color.primary};
+      `;
+    }
+  }}
+
+  ${(props) => props.$fullWidth && 'width: 100%;'}
+`;
