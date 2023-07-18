5c29b30a7604eb1c25e53e5626ac72e7be2cf818
jeongwusi <jeongwusi@gmail.com>
2023-07-18 11:21:58 +0900

    feat: MyProfile 페이지에 관련된 컴포넌트 조합하여 추가

--- client/src/pages/MyProfile.tsx
+++ client/src/pages/MyProfile.tsx
@@ -1,5 +1,45 @@
+import styled from 'styled-components';
+import Button from '../components/Button';
+import LikedCafeList from '../components/LikedCafeList';
+import Logo from '../components/Logo';
+import ProfileInfo from '../components/ProfileInfo';
+
 const MyProfile = () => {
-  return <div>MyProfile</div>;
+  return (
+    <Container>
+      <Logo fontSize="2xl" />
+      <ProfileInfo userImage="/images/profile-example.png" userName="김고니" />
+      <ButtonContainer>
+        <EditButtonContainer>
+          <Button $fullWidth={true}>프로필 수정하기</Button>
+        </EditButtonContainer>
+        <LogOutButton>
+          <Button variant="outlined" $fullWidth={true}>
+            로그아웃
+          </Button>
+        </LogOutButton>
+      </ButtonContainer>
+      <LikedCafeList />
+    </Container>
+  );
 };
+
+const Container = styled.section`
+  padding: ${({ theme }) => theme.space['7']} ${({ theme }) => theme.space['4']} 0 ${({ theme }) => theme.space['4']};
+`;
+
+const ButtonContainer = styled.article`
+  display: flex;
+  margin: ${({ theme }) => theme.space['3']} 0;
+`;
+
+const EditButtonContainer = styled.div`
+  width: 60%;
+  margin-right: ${({ theme }) => theme.space['2.5']};
+`;
+
+const LogOutButton = styled.div`
+  width: 40%;
+`;
 
 export default MyProfile;
