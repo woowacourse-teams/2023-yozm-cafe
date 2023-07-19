aaf0e8e3afae0c3ab14f64a978ca95a911eaa7d4
solo5star <solo_5@naver.com>
2023-07-19 15:42:52 +0900

    feat: Auth 페이지 구현

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,5 +1,8 @@
+import { Suspense } from 'react';
 import { createBrowserRouter } from 'react-router-dom';
+import Auth from './pages/Auth';
 import Home from './pages/Home';
+import Loading from './pages/Loading';
 import Login from './pages/Login';
 import MyProfile from './pages/MyProfile';
 import NotFound from './pages/NotFound';
@@ -15,6 +18,14 @@ const router = createBrowserRouter([
       { path: 'my-profile', element: <MyProfile /> },
       { path: 'login', element: <Login /> },
     ],
+  },
+  {
+    path: '/auth/:provider',
+    element: (
+      <Suspense fallback={<Loading />}>
+        <Auth />
+      </Suspense>
+    ),
   },
 ]);
 
