7a91a6570f80e6fdaae78432c2f46a5a5c67a56a
nuyh <hjaehyun25@gmail.com>
2023-07-19 15:09:35 +0900

    feat: 멤버 엔티티에 이름, 사진 컬럼 추가
    #48

--- server/src/main/java/com/project/yozmcafe/domain/member/Member.java
+++ server/src/main/java/com/project/yozmcafe/domain/member/Member.java
@@ -1,21 +1,23 @@
 package com.project.yozmcafe.domain.member;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.project.yozmcafe.domain.cafe.LikedCafe;
 import com.project.yozmcafe.domain.cafe.UnViewedCafe;
-
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Member {
 
     @Id
     private String id;
+    @Column(nullable = false)
     private String name;
+    @Column(nullable = false)
     private String image;
 
     @OneToMany(mappedBy = "member")
