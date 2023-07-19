756395a8f7812e611b8860c2e0f95cc65ee74ded
nuyh <hjaehyun25@gmail.com>
2023-07-19 22:19:06 +0900

    test: 토큰 Provider 테스트 추가
    #48

--- server/src/test/java/com/project/yozmcafe/service/auth/JwtTokenProviderTest.java
+++ server/src/test/java/com/project/yozmcafe/service/auth/JwtTokenProviderTest.java
@@ -4,36 +4,106 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class JwtTokenProviderTest {
 
     private final String testKey = "dsfjiaslejfilasjelfijsleifjliesajlif";
-    private final JwtTokenProvider jwtTokenProvider = new JwtTokenProvider(
+    private final JwtTokenProvider provider = new JwtTokenProvider(
             testKey,
-            1,
+            3600000,
             10210401);
+
+    @Test
+    @DisplayName("액세스 토큰 생성 테스트")
+    void createAccessToken() {
+        //given
+        final String token = provider.createAccess("멤버 아이디");
+
+        //when, then
+        assertDoesNotThrow(() -> provider.validate(token));
+    }
 
     @Test
     @DisplayName("리프레시 토큰 생성 테스트")
     void createRefreshToken() {
         //given
-        final String token = jwtTokenProvider.createRefresh();
+        final String token = provider.createRefresh();
 
         //when, then
-        assertDoesNotThrow(() -> jwtTokenProvider.validate(token));
+        assertDoesNotThrow(() -> provider.validate(token));
     }
 
     @Test
     @DisplayName("액세스 토큰에서 데이터 가져오기")
     void getPayload() {
         //given
-        final String token = jwtTokenProvider.createAccess("연어");
+        final String token = provider.createAccess("연어");
 
         //when
-        final String payload = jwtTokenProvider.getMemberId(token);
+        final String payload = provider.getMemberId(token);
 
         //then
         assertThat(payload).isEqualTo("연어");
+    }
+
+    @Test
+    @DisplayName("만료된 액세스 토큰에서 데이터 가져오기")
+    void getPayload2() {
+        //given
+        final JwtTokenProvider provider = new JwtTokenProvider(testKey, 1, 10210401);
+        final String token = provider.createAccess("연어");
+
+        //when
+        final String payload = provider.getMemberId(token);
+
+        //then
+        assertSoftly(softAssertions -> {
+            assertThat(payload).isEqualTo("연어");
+            assertThatThrownBy(() -> provider.validate(token))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("유효하지 않은 인증 정보입니다.");
+        });
+    }
+
+    @Test
+    @DisplayName("만료된 액세스 토큰으로 토큰 재생성")
+    void refreshAccessToken() {
+        //given
+        final JwtTokenProvider provider = new JwtTokenProvider(testKey, 1, 10210401);
+        final String access = provider.createAccess("연어");
+        final String refresh = provider.createRefresh();
+
+        //when
+        final String refreshedAccessToken = provider.refreshAccessToken(access, refresh);
+
+        //then
+        assertSoftly(softAssertions -> {
+            assertThatThrownBy(() -> provider.validate(access))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("유효하지 않은 인증 정보입니다.");
+            assertThat(provider.getMemberId(access))
+                    .isEqualTo(provider.getMemberId(refreshedAccessToken));
+        });
+    }
+
+    @Test
+    @DisplayName("만료되지 않은 액세스 토큰으로 토큰 재생성")
+    void refreshAccessToken2() {
+        //given
+        final String access = provider.createAccess("연어");
+        final String refresh = provider.createRefresh();
+
+        //when
+        final String refreshedAccessToken = provider.refreshAccessToken(access, refresh);
+
+        //then
+        assertSoftly(softAssertions -> {
+            assertDoesNotThrow(() -> provider.validate(access));
+            assertThat(provider.getMemberId(access))
+                    .isEqualTo(provider.getMemberId(refreshedAccessToken));
+        });
     }
 }
