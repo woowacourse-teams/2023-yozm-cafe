0561dd20c9a06bf9d2d077dbdc27b3042a4ccf70
jeongwusi <jeongwusi@users.noreply.github.com>
2023-07-20 10:37:27 +0000

    deploy: 637d483d68fa2734799d722a83d578f0a65d0fdc

--- components-CommentButton-stories.6232290e.iframe.bundle.js
+++
@@ -1,9 +0,0 @@
-"use strict";(self.webpackChunkyozm_cafe=self.webpackChunkyozm_cafe||[]).push([[795],{"./src/components/CommentButton.stories.tsx":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,{Default:()=>Default,__namedExportsOrder:()=>__namedExportsOrder,default:()=>CommentButton_stories});var index_esm=__webpack_require__("./node_modules/react-icons/fa/index.esm.js"),styled_components_browser_esm=__webpack_require__("./node_modules/styled-components/dist/styled-components.browser.esm.js"),jsx_runtime=__webpack_require__("./node_modules/react/jsx-runtime.js");const CommentButton=()=>(0,jsx_runtime.jsx)(Container,{children:(0,jsx_runtime.jsx)(Button,{onClick:()=>{alert("아직 준비중입니다!")}})});CommentButton.displayName="CommentButton";const components_CommentButton=CommentButton,Container=styled_components_browser_esm.zo.aside`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`,Button=(0,styled_components_browser_esm.zo)(index_esm.ZvA)`
-  cursor: pointer;
-  font-size: ${({theme})=>theme.fontSize["3xl"]};
-`,CommentButton_stories={title:"CommentButton",component:components_CommentButton},Default={args:{}};Default.parameters={...Default.parameters,docs:{...Default.parameters?.docs,source:{originalSource:"{\n  args: {}\n}",...Default.parameters?.docs?.source}}};const __namedExportsOrder=["Default"]}}]);
-//# sourceMappingURL=components-CommentButton-stories.6232290e.iframe.bundle.js.map
