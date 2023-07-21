8b87cf4dfc5bc1b6be8d064c79273ca060234c9e
geuntaek1013 <geuntaek1013@users.noreply.github.com>
2023-07-21 02:13:54 +0000

    deploy: 6f4ea4e09d59e0866698c054bb0fbce14e99e919

---
+++ components-CommentButton-stories.6bfee194.iframe.bundle.js
@@ -0,0 +1,11 @@
+"use strict";(self.webpackChunkyozm_cafe=self.webpackChunkyozm_cafe||[]).push([[795],{"./src/components/CommentButton.stories.tsx":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,{Default:()=>Default,__namedExportsOrder:()=>__namedExportsOrder,default:()=>CommentButton_stories});var index_esm=__webpack_require__("./node_modules/react-icons/pi/index.esm.js"),styled_components_browser_esm=__webpack_require__("./node_modules/styled-components/dist/styled-components.browser.esm.js"),jsx_runtime=__webpack_require__("./node_modules/react/jsx-runtime.js");const CommentButton=()=>(0,jsx_runtime.jsxs)(Container,{children:[(0,jsx_runtime.jsx)(Button,{onClick:()=>{alert("아직 준비중입니다!")}}),(0,jsx_runtime.jsx)(Text,{children:"댓글"})]});CommentButton.displayName="CommentButton";const components_CommentButton=CommentButton,Container=styled_components_browser_esm.zo.aside`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`,Button=(0,styled_components_browser_esm.zo)(index_esm.HIO)`
+  cursor: pointer;
+  font-size: ${({theme})=>theme.fontSize["5xl"]};
+`,Text=styled_components_browser_esm.zo.span`
+  font-size: ${({theme})=>theme.fontSize.sm};
+`,CommentButton_stories={title:"CommentButton",component:components_CommentButton},Default={args:{}};Default.parameters={...Default.parameters,docs:{...Default.parameters?.docs,source:{originalSource:"{\n  args: {}\n}",...Default.parameters?.docs?.source}}};const __namedExportsOrder=["Default"]}}]);
+//# sourceMappingURL=components-CommentButton-stories.6bfee194.iframe.bundle.js.map
--- components-ShareButton-stories.460feb0f.iframe.bundle.js
+++
@@ -1,9 +0,0 @@
-"use strict";(self.webpackChunkyozm_cafe=self.webpackChunkyozm_cafe||[]).push([[618],{"./src/components/ShareButton.stories.tsx":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,{Default:()=>Default,__namedExportsOrder:()=>__namedExportsOrder,default:()=>ShareButton_stories});var index_esm=__webpack_require__("./node_modules/react-icons/pi/index.esm.js"),styled_components_browser_esm=__webpack_require__("./node_modules/styled-components/dist/styled-components.browser.esm.js"),jsx_runtime=__webpack_require__("./node_modules/react/jsx-runtime.js");const ShareButton=()=>(0,jsx_runtime.jsx)(Container,{children:(0,jsx_runtime.jsx)(Button,{onClick:()=>{alert("아직 준비중입니다!")}})});ShareButton.displayName="ShareButton";const components_ShareButton=ShareButton,Container=styled_components_browser_esm.zo.aside`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`,Button=(0,styled_components_browser_esm.zo)(index_esm.v6z)`
-  cursor: pointer;
-  font-size: ${({theme})=>theme.fontSize["5xl"]};
-`,ShareButton_stories={title:"ShareButton",component:components_ShareButton},Default={args:{}};Default.parameters={...Default.parameters,docs:{...Default.parameters?.docs,source:{originalSource:"{\n  args: {}\n}",...Default.parameters?.docs?.source}}};const __namedExportsOrder=["Default"]}}]);
-//# sourceMappingURL=components-ShareButton-stories.460feb0f.iframe.bundle.js.map
