08f5499cb35c7319f17bbf64bfcfd6ae38f1472d
hum02 <ehdgma02@gmail.com>
2023-07-25 00:48:10 +0900

    refactor(CafeControllerTest): 불필요한 response extract() 삭제

--- server/src/test/java/com/project/yozmcafe/controller/CafeControllerTest.java
+++ server/src/test/java/com/project/yozmcafe/controller/CafeControllerTest.java
@@ -1,5 +1,6 @@
 package com.project.yozmcafe.controller;
 
+import com.project.yozmcafe.controller.dto.cafe.CafeResponse;
 import com.project.yozmcafe.domain.cafe.Cafe;
 import com.project.yozmcafe.domain.cafe.CafeRepository;
 import com.project.yozmcafe.domain.cafe.UnViewedCafe;
@@ -10,7 +11,6 @@ import com.project.yozmcafe.service.auth.JwtTokenProvider;
 import com.project.yozmcafe.util.AcceptanceContext;
 import com.project.yozmcafe.util.UnViewedCafeRepository;
 import io.restassured.RestAssured;
-import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -20,7 +20,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -48,6 +51,8 @@ class CafeControllerTest {
     @Autowired
     private UnViewedCafeRepository unViewedCafeCafeRepository;
 
+    private Member member;
+
     @BeforeEach
     void setUp() {
         cafeRepository.save(Fixture.getCafe("n1", "address1", 1));
@@ -58,22 +63,19 @@ class CafeControllerTest {
     }
 
     @Test
-    @DisplayName("로그인 되지 않은 사용자가 /cafes?page=? 에 GET요청을 보내면 페이지에 해당하는 카페 정보들을 응답한다.")
+    @DisplayName("로그인 되지 않은 사용자가 /cafes?page=? 에 GET요청을 보내면 페이지에 해당하는 서로 다른 카페 정보들을 5개씩 응답한다.")
     void getCafesSuccessByUnLoginUser() {
         //given
-        cafeRepository.save(Fixture.getCafe("n5", "daddre5", 1));
+        cafeRepository.save(Fixture.getCafe("n5", "address5", 1));
 
         //when
         context.invokeHttpGet("guest/cafes?page=1");
-        final ExtractableResponse<Response> response = context
-                .response
-                .then()
-                .extract();
 
         //then
+        Set<Long> cafeResponseIds = getCafeResponseIds(context.response);
         assertAll(
-                () -> assertThat(response.statusCode()).isEqualTo(200),
-                () -> assertThat(response.jsonPath().getList("$")).hasSize(5)
+                () -> assertThat(context.response.statusCode()).isEqualTo(200),
+                () -> assertThat(cafeResponseIds).hasSize(5)
         );
     }
 
@@ -82,42 +84,36 @@ class CafeControllerTest {
     void getCafesEmptyByUnLoginUser() {
         //when
         context.invokeHttpGet("guest/cafes?page=2000");
-        final ExtractableResponse<Response> response = context
-                .response
-                .then()
-                .extract();
 
         //then
         assertAll(
-                () -> assertThat(response.statusCode()).isEqualTo(200),
-                () -> assertThat(response.jsonPath().getList("$")).isEmpty()
+                () -> assertThat(context.response.statusCode()).isEqualTo(200),
+                () -> assertThat(context.response.jsonPath().getList("$")).isEmpty()
         );
     }
 
     @Test
-    @DisplayName("로그인한 사용자가 /cafes?page=?에 GET 요청을 보내면 아직보지않은 랜덤한 카페정보를 5개씩 응답한다.")
+    @DisplayName("로그인한 사용자가 /cafes?page=?에 GET 요청을 보내면 아직보지않은 랜덤한,서로 다른 카페정보를 5개씩 응답한다.")
     void getCafesWithMember() {
         //given
-        given(jwtTokenProvider.getMemberId(anyString())).willReturn("토큰");
         cafeRepository.save(Fixture.getCafe("n5", "address5", 1));
+        given(jwtTokenProvider.getMemberId(anyString())).willReturn("토큰");
         saveMemberAndUnViewedCafes();
 
         //when
         context.invokeHttpGetWithToken("/cafes?page=1");
-        final ExtractableResponse<Response> response = context
-                .response
-                .then()
-                .extract();
+        final Response response = context.response;
 
         //then
+        Set<Long> cafeIds = getCafeResponseIds(response);
         assertAll(
                 () -> assertThat(response.statusCode()).isEqualTo(200),
-                () -> assertThat(response.jsonPath().getList("$")).hasSize(5)
+                () -> assertThat(cafeIds).hasSize(5)
         );
     }
 
     @Test
-    @DisplayName("로그인한 사용자가 /cafes?page=?에 GET 요청을 보낼 때, 아직보지 않은 카페가 5개 미만이면 그 수만큼의 카페만 응답한다.")
+    @DisplayName("로그인한 사용자가 /cafes?page=?에 GET 요청을 보낼 때, 아직보지 않은 카페가 5개 미만이면 그 수만큼의 서로 다른 카페를 응답한다.")
     void getCafesWithMemberWhenCafeLessThan() {
         //given
         given(jwtTokenProvider.getMemberId(anyString())).willReturn("토큰");
@@ -125,27 +121,34 @@ class CafeControllerTest {
 
         //when
         context.invokeHttpGetWithToken("/cafes?page=1");
-        final ExtractableResponse<Response> response = context
-                .response
-                .then()
-                .extract();
+        Response response = context.response;
 
         //then
+        Set<Long> cafeIds = getCafeResponseIds(response);
         assertAll(
                 () -> assertThat(response.statusCode()).isEqualTo(200),
-                () -> assertThat(response.jsonPath().getList("$")).hasSize(4)
+                () -> assertThat(cafeIds).hasSize(4)
         );
     }
 
     private void saveMemberAndUnViewedCafes() {
-        Member member = memberRepository.save(new Member("토큰", "name1", "image"));
+        member = memberRepository.save(new Member("토큰", "name1", "image"));
 
         final List<Cafe> allCafes = cafeRepository.findAll();
         final List<UnViewedCafe> allUnViewedCafes = allCafes.stream()
-                .map(savedCafe -> new UnViewedCafe(null, savedCafe, member))
+                .map(savedCafe -> new UnViewedCafe(savedCafe, member))
                 .toList();
         unViewedCafeCafeRepository.saveAll(allUnViewedCafes);
 
         member.addUnViewedCafes(allCafes);
+    }
+
+    private static Set<Long> getCafeResponseIds(Response response) {
+        Set<Long> cafeIds = new HashSet<>();
+        List<CafeResponse> cafeResponses = response.jsonPath().getList(".", CafeResponse.class);
+
+        cafeIds.addAll(cafeResponses.stream().map(CafeResponse::id)
+                .collect(Collectors.toList()));
+        return cafeIds;
     }
 }
