d166901370342d50a03688e2eb16fd7bd48b6972
hum02 <ehdgma02@gmail.com>
2023-07-24 22:44:23 +0900

    refactor(CafeRepository): 메서드 파라미터의 final과 @Param 위치 수정

--- server/src/main/java/com/project/yozmcafe/domain/cafe/CafeRepository.java
+++ server/src/main/java/com/project/yozmcafe/domain/cafe/CafeRepository.java
@@ -1,12 +1,12 @@
 package com.project.yozmcafe.domain.cafe;
 
-import java.util.Optional;
-
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface CafeRepository extends JpaRepository<Cafe, Long> {
     Slice<Cafe> findSliceBy(Pageable pageable);
@@ -16,9 +16,9 @@ public interface CafeRepository extends JpaRepository<Cafe, Long> {
             + "INNER JOIN UnViewedCafe AS u "
             + "ON c.id = u.cafe.id "
             + "WHERE u.member.id = :memberId")
-    Slice<Cafe> findUnViewedCafesByMember(final Pageable pageable, final @Param("memberId") String memberId);
+    Slice<Cafe> findUnViewedCafesByMember(final Pageable pageable, @Param("memberId") final String memberId);
 
     @Query(value = "SELECT u FROM UnViewedCafe AS u "
             + "WHERE u.member.id = :memberId AND u.cafe.id = :cafeId")
-    Optional<UnViewedCafe> findUnViewedCafeByMemberAndCafe(final @Param("memberId") String memberId, final @Param("cafeId") Long cafeId);
+    Optional<UnViewedCafe> findUnViewedCafeByMemberAndCafe(@Param("memberId") final String memberId, @Param("cafeId") final Long cafeId);
 }
