9bc4f8605714039c87bded99639737284cdbce33
solo5star <solo_5@naver.com>
2023-07-25 12:40:12 +0900

    test: 카페 목록 API 모킹 방식 불변성으로 변경 및 좋아요 API 모킹

--- client/src/mocks/handlers.ts
+++ client/src/mocks/handlers.ts
@@ -1,45 +1,59 @@
 import { rest } from 'msw';
 import { cafes } from '../data/mockData';
 
+const PAGINATE_UNIT = 5;
+
 export const handlers = [
   // 카페 조회
   rest.get('/api/cafes', (req, res, ctx) => {
     const page = Number(req.url.searchParams.get('page') || 1);
+    const isPageExists = !!cafes.at(PAGINATE_UNIT * page - 1);
 
-    return res(
-      ctx.status(200),
-      ctx.json(
-        cafes.map((cafe) => ({
+    if (!isPageExists) {
+      cafes
+        .map((cafe) => structuredClone(cafe))
+        .map((cafe, index) => ({
           ...cafe,
-          id: cafes.length * Number(page - 1) + cafe.id,
-          name: `${cafe.name} (${page}번째 지점)`,
-        })),
-      ),
-    );
+          name: `${cafe.name} (${page}호점)`,
+          id: PAGINATE_UNIT * (page - 1) + 1 + index,
+        }))
+        .forEach((cafe) => {
+          cafes.push(cafe);
+        });
+    }
+
+    const [start, end] = [PAGINATE_UNIT * (page - 1), PAGINATE_UNIT * page];
+    const paginatedCafes = cafes.slice(start, end);
+
+    return res(ctx.status(200), ctx.json(paginatedCafes));
   }),
 
   // 좋아요 추가
-  rest.post('/api/cafes/:cafe_id/likes', (req, res, ctx) => {
+  rest.post('/api/cafes/:cafeId/likes', (req, res, ctx) => {
     const { cafeId } = req.params;
-    const updatedCafes = cafes.map((cafe) => {
-      if (cafe.id === Number(cafeId)) {
-        return { ...cafe, likeCount: cafe.likeCount + 1, isLiked: true };
-      }
-      return cafe;
-    });
-    return res(ctx.status(200), ctx.json(updatedCafes));
-  }),
+    const liked = req.url.searchParams.get('isLiked');
+    if (!liked) {
+      return res(
+        ctx.status(400),
+        ctx.json({
+          message: 'isLiked Query Parameter가 필요합니다.',
+        }),
+      );
+    }
+    const isLiked = liked === 'true';
 
-  // 좋아요 취소
-  rest.delete('/api/cafes/:cafe_id/likes', (req, res, ctx) => {
-    const { cafeId } = req.params;
-    const updatedCafes = cafes.map((cafe) => {
-      if (cafe.id === Number(cafeId) && cafe.likeCount > 0) {
-        return { ...cafe, likeCount: cafe.likeCount - 1, isLiked: false };
-      }
-      return cafe;
-    });
-    return res(ctx.status(200), ctx.json(updatedCafes));
+    const cafe = cafes.find((cafe) => cafe.id === Number(cafeId));
+    if (!cafe) {
+      return res(
+        ctx.status(404),
+        ctx.json({
+          message: '카페를 찾을 수 없습니다.',
+        }),
+      );
+    }
+
+    cafe.isLiked = isLiked;
+    return res(ctx.status(200), ctx.json({}));
   }),
 
   // 인증 코드를 accessToken으로 교환
