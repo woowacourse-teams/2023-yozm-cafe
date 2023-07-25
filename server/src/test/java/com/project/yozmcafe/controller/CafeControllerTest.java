dcb7367a5f4e1418dbeb15e1d4affd5a9b937386
hum02 <ehdgma02@gmail.com>
2023-07-25 10:28:07 +0900

    test(CafeControllerTest):카페 정보 조회한 후 값에 대한 검증 추가

--- server/src/test/java/com/project/yozmcafe/controller/CafeControllerTest.java
+++ server/src/test/java/com/project/yozmcafe/controller/CafeControllerTest.java
@@ -11,25 +11,25 @@ import com.project.yozmcafe.service.auth.JwtTokenProvider;
 import com.project.yozmcafe.util.AcceptanceContext;
 import com.project.yozmcafe.util.UnViewedCafeRepository;
 import io.restassured.RestAssured;
-import io.restassured.response.Response;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.groups.Tuple.tuple;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CafeControllerTest {
 
@@ -51,31 +51,42 @@ class CafeControllerTest {
     @Autowired
     private UnViewedCafeRepository unViewedCafeCafeRepository;
 
-    private Member member;
+    private Cafe cafe1, cafe2, cafe3, cafe4, cafe5;
 
     @BeforeEach
     void setUp() {
-        cafeRepository.save(Fixture.getCafe("n1", "address1", 1));
-        cafeRepository.save(Fixture.getCafe("n2", "address2", 1));
-        cafeRepository.save(Fixture.getCafe("n3", "address3", 1));
-        cafeRepository.save(Fixture.getCafe("n4", "address4", 1));
+        cafe1 = cafeRepository.save(Fixture.getCafe("n1", "address1", 1));
+        cafe2 = cafeRepository.save(Fixture.getCafe("n2", "address2", 1));
+        cafe3 = cafeRepository.save(Fixture.getCafe("n3", "address3", 1));
+        cafe4 = cafeRepository.save(Fixture.getCafe("n4", "address4", 1));
         RestAssured.port = port;
+    }
+
+    @AfterEach
+    public void afterEach() {
+        memberRepository.deleteAll();
+        cafeRepository.deleteAll();
     }
 
     @Test
     @DisplayName("로그인 되지 않은 사용자가 /cafes?page=? 에 GET요청을 보내면 페이지에 해당하는 서로 다른 카페 정보들을 5개씩 응답한다.")
     void getCafesSuccessByUnLoginUser() {
         //given
-        cafeRepository.save(Fixture.getCafe("n5", "address5", 1));
+        cafe5 = cafeRepository.save(Fixture.getCafe("n5", "address5", 1));
 
         //when
         context.invokeHttpGet("guest/cafes?page=1");
 
         //then
-        Set<Long> cafeResponseIds = getCafeResponseIds(context.response);
+        List<CafeResponse> cafeResponses = context.response.jsonPath().getList(".", CafeResponse.class);
         assertAll(
                 () -> assertThat(context.response.statusCode()).isEqualTo(200),
-                () -> assertThat(cafeResponseIds).hasSize(5)
+                () -> assertThat(cafeResponses).extracting("id", "name")
+                        .contains(tuple(cafe1.getId(), "n1"),
+                                tuple(cafe2.getId(), "n2"),
+                                tuple(cafe3.getId(), "n3"),
+                                tuple(cafe4.getId(), "n4"),
+                                tuple(cafe5.getId(), "n5"))
         );
     }
 
@@ -96,19 +107,24 @@ class CafeControllerTest {
     @DisplayName("로그인한 사용자가 /cafes?page=?에 GET 요청을 보내면 아직보지않은 랜덤한,서로 다른 카페정보를 5개씩 응답한다.")
     void getCafesWithMember() {
         //given
-        cafeRepository.save(Fixture.getCafe("n5", "address5", 1));
+        cafe5 = cafeRepository.save(Fixture.getCafe("n5", "address5", 1));
         given(jwtTokenProvider.getMemberId(anyString())).willReturn("토큰");
         saveMemberAndUnViewedCafes();
 
         //when
         context.invokeHttpGetWithToken("/cafes?page=1");
-        final Response response = context.response;
 
         //then
-        Set<Long> cafeIds = getCafeResponseIds(response);
+        List<CafeResponse> cafeResponses = context.response.jsonPath().getList(".", CafeResponse.class);
+
         assertAll(
-                () -> assertThat(response.statusCode()).isEqualTo(200),
-                () -> assertThat(cafeIds).hasSize(5)
+                () -> assertThat(context.response.statusCode()).isEqualTo(200),
+                () -> assertThat(cafeResponses).extracting("id", "name")
+                        .contains(tuple(cafe1.getId(), "n1"),
+                                tuple(cafe2.getId(), "n2"),
+                                tuple(cafe3.getId(), "n3"),
+                                tuple(cafe4.getId(), "n4"),
+                                tuple(cafe5.getId(), "n5"))
         );
     }
 
@@ -121,18 +137,22 @@ class CafeControllerTest {
 
         //when
         context.invokeHttpGetWithToken("/cafes?page=1");
-        Response response = context.response;
 
         //then
-        Set<Long> cafeIds = getCafeResponseIds(response);
+        List<CafeResponse> cafeResponses = context.response.jsonPath().getList(".", CafeResponse.class);
+
         assertAll(
-                () -> assertThat(response.statusCode()).isEqualTo(200),
-                () -> assertThat(cafeIds).hasSize(4)
+                () -> assertThat(context.response.statusCode()).isEqualTo(200),
+                () -> assertThat(cafeResponses).extracting("id", "name")
+                        .contains(tuple(cafe1.getId(), "n1"),
+                                tuple(cafe2.getId(), "n2"),
+                                tuple(cafe3.getId(), "n3"),
+                                tuple(cafe4.getId(), "n4"))
         );
     }
 
     private void saveMemberAndUnViewedCafes() {
-        member = memberRepository.save(new Member("토큰", "name1", "image"));
+        Member member = memberRepository.save(new Member("토큰", "name1", "image"));
 
         final List<Cafe> allCafes = cafeRepository.findAll();
         final List<UnViewedCafe> allUnViewedCafes = allCafes.stream()
@@ -141,14 +161,5 @@ class CafeControllerTest {
         unViewedCafeCafeRepository.saveAll(allUnViewedCafes);
 
         member.addUnViewedCafes(allCafes);
-    }
-
-    private static Set<Long> getCafeResponseIds(Response response) {
-        Set<Long> cafeIds = new HashSet<>();
-        List<CafeResponse> cafeResponses = response.jsonPath().getList(".", CafeResponse.class);
-
-        cafeIds.addAll(cafeResponses.stream().map(CafeResponse::id)
-                .collect(Collectors.toList()));
-        return cafeIds;
     }
 }
