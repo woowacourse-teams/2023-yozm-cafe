ef7e833ec3a3b1bd937d56a26043bd63c65585fd
jeongwusi <jeongwusi@users.noreply.github.com>
2023-07-25 23:55:33 +0000

    deploy: 679d54b1ba86911ea2f41ec6ce457a72012e8fda

---
+++ components-Button-stories.1acb6502.iframe.bundle.js
@@ -0,0 +1,26 @@
+"use strict";(self.webpackChunkyozm_cafe=self.webpackChunkyozm_cafe||[]).push([[706],{"./src/components/Button.stories.tsx":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,{Default:()=>Default,__namedExportsOrder:()=>__namedExportsOrder,default:()=>__WEBPACK_DEFAULT_EXPORT__});const __WEBPACK_DEFAULT_EXPORT__={title:"Button",component:__webpack_require__("./src/components/Button.tsx").Z},Default={args:{children:"버튼",fullWidth:!0}};Default.parameters={...Default.parameters,docs:{...Default.parameters?.docs,source:{originalSource:"{\n  args: {\n    children: '버튼',\n    fullWidth: true\n  }\n}",...Default.parameters?.docs?.source}}};const __namedExportsOrder=["Default"]},"./src/components/Button.tsx":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{__webpack_require__.d(__webpack_exports__,{Z:()=>__WEBPACK_DEFAULT_EXPORT__});var styled_components__WEBPACK_IMPORTED_MODULE_1__=__webpack_require__("./node_modules/styled-components/dist/styled-components.browser.esm.js"),react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__=__webpack_require__("./node_modules/react/jsx-runtime.js");const Button=({children,variant="default",fullWidth=!1,fullHeight=!1,...rest})=>(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(Container,{variant,fullWidth,fullHeight,...rest,children});Button.displayName="Button";const __WEBPACK_DEFAULT_EXPORT__=Button,ButtonVariants={disabled:styled_components__WEBPACK_IMPORTED_MODULE_1__.iv`
+    color: ${props=>props.theme.color.white};
+    background-color: ${props=>props.theme.color.gray};
+    border: none;
+  `,outlined:styled_components__WEBPACK_IMPORTED_MODULE_1__.iv`
+    color: ${props=>props.theme.color.gray};
+    background-color: ${props=>props.theme.color.white};
+    border: 2px solid ${props=>props.theme.color.primary};
+  `,default:styled_components__WEBPACK_IMPORTED_MODULE_1__.iv`
+    color: ${props=>props.theme.color.white};
+    background-color: ${props=>props.theme.color.primary};
+    border: none;
+  `},Container=styled_components__WEBPACK_IMPORTED_MODULE_1__.ZP.button`
+  cursor: pointer;
+
+  padding: ${({theme})=>theme.space[1.5]} 0;
+
+  font-size: 16px;
+  font-weight: 500;
+
+  border-radius: 40px;
+  ${props=>ButtonVariants[props.variant||"default"]}
+  ${props=>props.fullWidth&&"width: 100%;"}
+  ${props=>props.fullHeight&&"height: 100%;"}
+`;try{Button.displayName="Button",Button.__docgenInfo={description:"",displayName:"Button",props:{variant:{defaultValue:{value:"default"},description:"",name:"variant",required:!1,type:{name:"enum",value:[{value:'"disabled"'},{value:'"default"'},{value:'"outlined"'}]}},fullWidth:{defaultValue:{value:"false"},description:"",name:"fullWidth",required:!1,type:{name:"boolean"}},fullHeight:{defaultValue:{value:"false"},description:"",name:"fullHeight",required:!1,type:{name:"boolean"}}}},"undefined"!=typeof STORYBOOK_REACT_CLASSES&&(STORYBOOK_REACT_CLASSES["src/components/Button.tsx#Button"]={docgenInfo:Button.__docgenInfo,name:"Button",path:"src/components/Button.tsx#Button"})}catch(__react_docgen_typescript_loader_error){}}}]);
+//# sourceMappingURL=components-Button-stories.1acb6502.iframe.bundle.js.map
---
+++ components-Navbar-stories.99c20782.iframe.bundle.js
@@ -0,0 +1,45 @@
+/*! For license information please see components-Navbar-stories.99c20782.iframe.bundle.js.LICENSE.txt */
+"use strict";(self.webpackChunkyozm_cafe=self.webpackChunkyozm_cafe||[]).push([[425],{"./src/components/Navbar.stories.tsx":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,{Default:()=>Default,__namedExportsOrder:()=>__namedExportsOrder,default:()=>Navbar_stories});var react=__webpack_require__("./node_modules/react/index.js"),react_namespaceObject=__webpack_require__.t(react,2),dist=__webpack_require__("./node_modules/react-router/dist/index.js"),router=__webpack_require__("./node_modules/@remix-run/router/dist/router.js");function _extends(){return _extends=Object.assign?Object.assign.bind():function(target){for(var i=1;i<arguments.length;i++){var source=arguments[i];for(var key in source)Object.prototype.hasOwnProperty.call(source,key)&&(target[key]=source[key])}return target},_extends.apply(this,arguments)}function _objectWithoutPropertiesLoose(source,excluded){if(null==source)return{};var key,i,target={},sourceKeys=Object.keys(source);for(i=0;i<sourceKeys.length;i++)key=sourceKeys[i],excluded.indexOf(key)>=0||(target[key]=source[key]);return target}new Set(["application/x-www-form-urlencoded","multipart/form-data","text/plain"]);const _excluded=["onClick","relative","reloadDocument","replace","state","target","to","preventScrollReset"];react_namespaceObject.startTransition;const isBrowser="undefined"!=typeof window&&void 0!==window.document&&void 0!==window.document.createElement,ABSOLUTE_URL_REGEX=/^(?:[a-z][a-z0-9+.-]*:|\/\/)/i,Link=react.forwardRef((function LinkWithRef(_ref4,ref){let absoluteHref,{onClick,relative,reloadDocument,replace,state,target,to,preventScrollReset}=_ref4,rest=_objectWithoutPropertiesLoose(_ref4,_excluded),{basename}=react.useContext(dist.Us),isExternal=!1;if("string"==typeof to&&ABSOLUTE_URL_REGEX.test(to)&&(absoluteHref=to,isBrowser))try{let currentUrl=new URL(window.location.href),targetUrl=to.startsWith("//")?new URL(currentUrl.protocol+to):new URL(to),path=(0,router.Zn)(targetUrl.pathname,basename);targetUrl.origin===currentUrl.origin&&null!=path?to=path+targetUrl.search+targetUrl.hash:isExternal=!0}catch(e){}let href=(0,dist.oQ)(to,{relative}),internalOnClick=function useLinkClickHandler(to,_temp){let{target,replace:replaceProp,state,preventScrollReset,relative}=void 0===_temp?{}:_temp,navigate=(0,dist.s0)(),location=(0,dist.TH)(),path=(0,dist.WU)(to,{relative});return react.useCallback((event=>{if(function shouldProcessLinkClick(event,target){return!(0!==event.button||target&&"_self"!==target||function isModifiedEvent(event){return!!(event.metaKey||event.altKey||event.ctrlKey||event.shiftKey)}(event))}(event,target)){event.preventDefault();let replace=void 0!==replaceProp?replaceProp:(0,router.Ep)(location)===(0,router.Ep)(path);navigate(to,{replace,state,preventScrollReset,relative})}}),[location,navigate,path,replaceProp,state,target,to,preventScrollReset,relative])}(to,{replace,state,target,preventScrollReset,relative});return react.createElement("a",_extends({},rest,{href:absoluteHref||href,onClick:isExternal||reloadDocument?onClick:function handleClick(event){onClick&&onClick(event),event.defaultPrevented||internalOnClick(event)},ref,target}))}));var DataRouterHook,DataRouterStateHook;(function(DataRouterHook){DataRouterHook.UseScrollRestoration="useScrollRestoration",DataRouterHook.UseSubmit="useSubmit",DataRouterHook.UseSubmitFetcher="useSubmitFetcher",DataRouterHook.UseFetcher="useFetcher"})(DataRouterHook||(DataRouterHook={})),function(DataRouterStateHook){DataRouterStateHook.UseFetchers="useFetchers",DataRouterStateHook.UseScrollRestoration="useScrollRestoration"}(DataRouterStateHook||(DataRouterStateHook={}));var styled_components_browser_esm=__webpack_require__("./node_modules/styled-components/dist/styled-components.browser.esm.js"),Button=__webpack_require__("./src/components/Button.tsx"),Logo=__webpack_require__("./src/components/Logo.tsx"),jsx_runtime=__webpack_require__("./node_modules/react/jsx-runtime.js");const Navbar=()=>(0,jsx_runtime.jsxs)(Container,{children:[(0,jsx_runtime.jsx)(LogoContainer,{to:"/",children:(0,jsx_runtime.jsx)(Logo.Z,{fontSize:"4xl"})}),(0,jsx_runtime.jsx)(ButtonContainer,{children:(0,jsx_runtime.jsx)(Button.Z,{fullWidth:!0,fullHeight:!0,children:"로그인"})})]});Navbar.displayName="Navbar";const components_Navbar=Navbar,Container=styled_components_browser_esm.zo.nav`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  width: 100%;
+  height: 66px;
+  padding: 0 15px;
+`,ButtonContainer=styled_components_browser_esm.zo.div`
+  flex: 2;
+  height: 55%;
+`,LogoContainer=(0,styled_components_browser_esm.zo)(Link)`
+  flex: 8;
+  text-decoration: none;
+`,Navbar_stories={title:"Navbar",component:components_Navbar},Default={args:{}};Default.parameters={...Default.parameters,docs:{...Default.parameters?.docs,source:{originalSource:"{\n  args: {}\n}",...Default.parameters?.docs?.source}}};const __namedExportsOrder=["Default"]},"./src/components/Button.tsx":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{__webpack_require__.d(__webpack_exports__,{Z:()=>__WEBPACK_DEFAULT_EXPORT__});var styled_components__WEBPACK_IMPORTED_MODULE_1__=__webpack_require__("./node_modules/styled-components/dist/styled-components.browser.esm.js"),react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__=__webpack_require__("./node_modules/react/jsx-runtime.js");const Button=({children,variant="default",fullWidth=!1,fullHeight=!1,...rest})=>(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(Container,{variant,fullWidth,fullHeight,...rest,children});Button.displayName="Button";const __WEBPACK_DEFAULT_EXPORT__=Button,ButtonVariants={disabled:styled_components__WEBPACK_IMPORTED_MODULE_1__.iv`
+    color: ${props=>props.theme.color.white};
+    background-color: ${props=>props.theme.color.gray};
+    border: none;
+  `,outlined:styled_components__WEBPACK_IMPORTED_MODULE_1__.iv`
+    color: ${props=>props.theme.color.gray};
+    background-color: ${props=>props.theme.color.white};
+    border: 2px solid ${props=>props.theme.color.primary};
+  `,default:styled_components__WEBPACK_IMPORTED_MODULE_1__.iv`
+    color: ${props=>props.theme.color.white};
+    background-color: ${props=>props.theme.color.primary};
+    border: none;
+  `},Container=styled_components__WEBPACK_IMPORTED_MODULE_1__.ZP.button`
+  cursor: pointer;
+
+  padding: ${({theme})=>theme.space[1.5]} 0;
+
+  font-size: 16px;
+  font-weight: 500;
+
+  border-radius: 40px;
+  ${props=>ButtonVariants[props.variant||"default"]}
+  ${props=>props.fullWidth&&"width: 100%;"}
+  ${props=>props.fullHeight&&"height: 100%;"}
+`;try{Button.displayName="Button",Button.__docgenInfo={description:"",displayName:"Button",props:{variant:{defaultValue:{value:"default"},description:"",name:"variant",required:!1,type:{name:"enum",value:[{value:'"disabled"'},{value:'"default"'},{value:'"outlined"'}]}},fullWidth:{defaultValue:{value:"false"},description:"",name:"fullWidth",required:!1,type:{name:"boolean"}},fullHeight:{defaultValue:{value:"false"},description:"",name:"fullHeight",required:!1,type:{name:"boolean"}}}},"undefined"!=typeof STORYBOOK_REACT_CLASSES&&(STORYBOOK_REACT_CLASSES["src/components/Button.tsx#Button"]={docgenInfo:Button.__docgenInfo,name:"Button",path:"src/components/Button.tsx#Button"})}catch(__react_docgen_typescript_loader_error){}},"./src/components/Logo.tsx":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{__webpack_require__.d(__webpack_exports__,{Z:()=>__WEBPACK_DEFAULT_EXPORT__});var styled_components__WEBPACK_IMPORTED_MODULE_2__=__webpack_require__("./node_modules/styled-components/dist/styled-components.browser.esm.js"),_styles_theme__WEBPACK_IMPORTED_MODULE_0__=__webpack_require__("./src/styles/theme.ts"),react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__=__webpack_require__("./node_modules/react/jsx-runtime.js");const Logo=({fontSize})=>(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(StyledLogo,{fontSize,children:"요즘카페"});Logo.displayName="Logo";const __WEBPACK_DEFAULT_EXPORT__=Logo,StyledLogo=styled_components__WEBPACK_IMPORTED_MODULE_2__.zo.h1`
+  font-family: 'BMJUA', sans-serif;
+  font-size: ${props=>_styles_theme__WEBPACK_IMPORTED_MODULE_0__.Z.fontSize[props.fontSize]};
+  color: ${({theme})=>theme.color.primary};
+`;try{Logo.displayName="Logo",Logo.__docgenInfo={description:"",displayName:"Logo",props:{fontSize:{defaultValue:null,description:"",name:"fontSize",required:!0,type:{name:"enum",value:[{value:'"xs"'},{value:'"sm"'},{value:'"base"'},{value:'"lg"'},{value:'"xl"'},{value:'"2xl"'},{value:'"3xl"'},{value:'"4xl"'},{value:'"5xl"'},{value:'"6xl"'},{value:'"7xl"'}]}}}},"undefined"!=typeof STORYBOOK_REACT_CLASSES&&(STORYBOOK_REACT_CLASSES["src/components/Logo.tsx#Logo"]={docgenInfo:Logo.__docgenInfo,name:"Logo",path:"src/components/Logo.tsx#Logo"})}catch(__react_docgen_typescript_loader_error){}}}]);
+//# sourceMappingURL=components-Navbar-stories.99c20782.iframe.bundle.js.map
