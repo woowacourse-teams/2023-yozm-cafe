536189d4bd03bc0418fb70170fc4f1acab12c72a
dev_kong <ahdjd5@gmail.com>
2023-07-25 19:30:52 +0900

    feat: MemberService#updateLike 멤버의 좋아요 목록을 업데이트 하는 기능 구현

--- server/src/main/java/com/project/yozmcafe/service/MemberService.java
+++ server/src/main/java/com/project/yozmcafe/service/MemberService.java
@@ -1,19 +1,24 @@
 package com.project.yozmcafe.service;
 
-import com.project.yozmcafe.controller.dto.MemberResponse;
-import com.project.yozmcafe.domain.member.Member;
-import com.project.yozmcafe.domain.member.MemberRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.project.yozmcafe.controller.dto.MemberResponse;
+import com.project.yozmcafe.domain.cafe.Cafe;
+import com.project.yozmcafe.domain.cafe.CafeRepository;
+import com.project.yozmcafe.domain.member.Member;
+import com.project.yozmcafe.domain.member.MemberRepository;
 
 @Transactional(readOnly = true)
 @Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final CafeRepository cafeRepository;
 
-    public MemberService(final MemberRepository memberRepository) {
+    public MemberService(final MemberRepository memberRepository, final CafeRepository cafeRepository) {
         this.memberRepository = memberRepository;
+        this.cafeRepository = cafeRepository;
     }
 
     public MemberResponse findById(final String id) {
@@ -21,5 +26,12 @@ public class MemberService {
                 .orElseThrow(() -> new IllegalArgumentException("해당하는 회원이 존재하지 않습니다."));
 
         return MemberResponse.from(member);
+    }
+
+    @Transactional
+    public void updateLike(final Member member, final long cafeId, final boolean isLiked) {
+        final Cafe cafe = cafeRepository.findById(cafeId)
+                .orElseThrow(() -> new IllegalArgumentException("해당하는 카페가 존재하지 않습니다."));
+        member.updateIsLike(cafe, isLiked);
     }
 }
