80bf99420989d64c2ed308a91a7bbbb26d335354
nuyh <hjaehyun25@gmail.com>
2023-07-25 13:24:42 +0900

    fix: SpringBootTest에 롤백 적용

--- server/src/test/java/com/project/yozmcafe/service/MemberServiceTest.java
+++ server/src/test/java/com/project/yozmcafe/service/MemberServiceTest.java
@@ -7,11 +7,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
+@Transactional
 class MemberServiceTest {
 
     @Autowired
