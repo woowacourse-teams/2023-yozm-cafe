d6f09bb1f12000e0a836606c18f8cfda3bd081b3
donghae-kim <donghae1999@gmail.com>
2023-07-26 14:25:13 +0900

    test: 좋아요 목록 조회 테스트 추가
    #115

---
+++ server/src/test/java/com/project/yozmcafe/domain/cafe/ImagesTest.java
@@ -0,0 +1,38 @@
+package com.project.yozmcafe.domain.cafe;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class ImagesTest {
+
+    @Test
+    @DisplayName("대표 이미지를 가져온다.")
+    void getRepresentativeImage() {
+        //given
+        final List<String> urls = List.of("오션.img", "연어.img", "요즘카페.img");
+        final Images images = new Images(urls);
+
+        //when
+        final String representativeImage = images.getRepresentativeImage();
+
+        //then
+        assertThat(representativeImage).isEqualTo("오션.img");
+    }
+
+    @Test
+    @DisplayName("대표 이미지를 조회하는데 이미지가 없을 경우 예외가 발생한다.")
+    void getRepresentativeImage_fail() {
+        //given
+        final Images images = new Images(Collections.emptyList());
+
+        //when
+        //then
+        assertThatThrownBy(images::getRepresentativeImage).isInstanceOf(IllegalArgumentException.class);
+    }
+}
