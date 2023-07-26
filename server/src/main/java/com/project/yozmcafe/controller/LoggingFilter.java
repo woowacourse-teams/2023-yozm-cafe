80b535a725e012ab033789b3d5d05e882ecbc7b3
nuyh <hjaehyun25@gmail.com>
2023-07-26 15:29:13 +0900

    feat: 로깅 기능 추가
    #130

---
+++ server/src/main/java/com/project/yozmcafe/controller/LoggingFilter.java
@@ -0,0 +1,46 @@
+package com.project.yozmcafe.controller;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Objects;
+import java.util.UUID;
+
+@Component
+public class LoggingFilter extends OncePerRequestFilter {
+
+    private static final String AUTHORIZATION = "Authorization";
+
+    private static final Logger logger = LoggerFactory.getLogger("API Request");
+    private static final String KEY = "user";
+    private static final String ANONYMOUS = "anonymous";
+
+    @Override
+    protected void doFilterInternal(final HttpServletRequest request,
+                                    final HttpServletResponse response,
+                                    final FilterChain filterChain) throws ServletException, IOException {
+        MDC.put(KEY, getUserFrom(request));
+        logger.info("Request For '{}'", request.getRequestURI());
+        MDC.clear();
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String getUserFrom(final HttpServletRequest request) {
+        final String authorization = request.getHeader(AUTHORIZATION);
+
+        if (Objects.isNull(authorization)) {
+            return ANONYMOUS;
+        }
+
+        return UUID.nameUUIDFromBytes(authorization.getBytes()).toString();
+    }
+}
