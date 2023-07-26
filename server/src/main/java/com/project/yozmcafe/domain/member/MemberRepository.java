98ec9bb34e0bd9b1721eda2bceae1787721ab88c
donghae-kim <donghae1999@gmail.com>
2023-07-26 14:24:37 +0900

    feat: 좋아요 목록 조회 기능 추가
    #115

--- server/src/main/java/com/project/yozmcafe/domain/member/MemberRepository.java
+++ server/src/main/java/com/project/yozmcafe/domain/member/MemberRepository.java
@@ -1,6 +1,11 @@
 package com.project.yozmcafe.domain.member;
 
+import com.project.yozmcafe.domain.cafe.LikedCafe;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -8,4 +13,7 @@ import java.util.Optional;
 @Repository
 public interface MemberRepository extends JpaRepository<Member, String> {
     Optional<Member> findById(String id);
+
+    @Query("SELECT likedCafes FROM Member m JOIN m.likedCafes likedCafes WHERE m.id = :memberId")
+    Slice<LikedCafe> findLikedCafeByMemberId(@Param("memberId") final String memberId, final Pageable pageable);
 }
