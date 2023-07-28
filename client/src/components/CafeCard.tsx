a4400305a7b4f1d59f0cc07dd254d95f1e8ebe95
jeongwusi <jeongwusi@gmail.com>
2023-07-28 10:21:34 +0900

    feat: 이미지 캐러셀 기능 추가

--- client/src/components/CafeCard.tsx
+++ client/src/components/CafeCard.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
 import { styled } from 'styled-components';
 import useIntersection from '../hooks/useIntersection';
 import { Cafe } from '../types';
@@ -12,6 +13,7 @@ type CardProps = {
 
 const CafeCard = (props: CardProps) => {
   const { cafe, onIntersect } = props;
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
   const ref = useRef<HTMLImageElement>(null);
   const intersection = useIntersection(ref, { threshold: 0.7 });
@@ -22,9 +24,28 @@ const CafeCard = (props: CardProps) => {
     }
   }, [intersection?.isIntersecting, onIntersect]);
 
+  const handlePrevImage = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex === 0 ? cafe.images.urls.length - 1 : prevIndex - 1));
+  };
+
+  const handleNextImage = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex === cafe.images.urls.length - 1 ? 0 : prevIndex + 1));
+  };
+
+  const renderDots = () => {
+    return cafe.images.urls.map((_, index) => (
+      <Dot key={index} active={index === currentImageIndex} onClick={() => setCurrentImageIndex(index)} />
+    ));
+  };
+
   return (
     <Container>
-      <Image ref={ref} src={cafe.images.urls[0]} />
+      <CarouselImage ref={ref} src={cafe.images.urls[currentImageIndex]} />
+      <DotsContainer>{renderDots()}</DotsContainer>
+      <CarouselNavigation>
+        <BsChevronCompactLeft onClick={handlePrevImage} />
+        <BsChevronCompactRight onClick={handleNextImage} />
+      </CarouselNavigation>
       <AsidePosition>
         <Aside>
           <CafeInfoModal title={cafe.name} address={cafe.address} content={cafe.detail.description} />
@@ -43,10 +64,48 @@ const Container = styled.div`
   height: 100%;
 `;
 
-const Image = styled.img`
+const CarouselImage = styled.img`
+  overflow: hidden;
   width: 100%;
   height: 100%;
   object-fit: cover;
+`;
+
+const CarouselNavigation = styled.div`
+  position: absolute;
+  z-index: 2;
+  top: 50%;
+
+  display: flex;
+  justify-content: space-between;
+
+  width: 100%;
+  & > * {
+    cursor: pointer;
+    font-size: ${({ theme }) => theme.fontSize['3xl']};
+    color: ${({ theme }) => theme.color.white};
+  }
+`;
+
+const DotsContainer = styled.div`
+  position: absolute;
+  z-index: 2;
+  bottom: ${({ theme }) => theme.space[5]};
+  left: 50%;
+  transform: translateX(-50%);
+
+  display: flex;
+  gap: ${({ theme }) => theme.space[2]};
+`;
+
+const Dot = styled.div<{ active: boolean }>`
+  cursor: pointer;
+
+  width: 8px;
+  height: 8px;
+
+  background-color: ${({ active, theme }) => (active ? theme.color.white : theme.color.gray)};
+  border-radius: 50%;
 `;
 
 const AsidePosition = styled.div`
@@ -57,7 +116,10 @@ const AsidePosition = styled.div`
 
 const Aside = styled.div`
   position: relative;
+
   display: flex;
   flex-direction: column-reverse;
-  gap: ${({ theme }) => theme.space[20]};
+  gap: ${({ theme }) => theme.space[10]};
+
+  margin: ${({ theme }) => theme.space[5]};
 `;
