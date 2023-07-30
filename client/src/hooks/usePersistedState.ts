a0f9e67872de1c78510b6174ac7785d0b0d70f40
solo5star <solo_5@naver.com>
2023-07-30 21:12:39 +0900

    feat: 로그인 상태가 영구적으로 저장되도록 accessToken 상태를 `usePersistedState` 훅을 이용하여 보관

---
+++ client/src/hooks/usePersistedState.ts
@@ -0,0 +1,34 @@
+import { Dispatch, useEffect, useState } from 'react';
+
+/**
+ * useState와 localStorage가 결합된 훅입니다. 영구 저장되는 상태를 이용할 수 있습니다.
+ * 상태 값은 `JSON.stringify` 및 `JSON.parse` 가능한 값으로 사용하여야 합니다.
+ *
+ * @example
+ * const Example = () => {
+ *   const [accessToken, setAccessToken] = usePersistedState(null);
+ *
+ *   const onLogin = () => {
+ *     login().then((response) => setAccessToken(response.data.accessToken));
+ *   }
+ *
+ *   return (
+ *     // ...
+ *   )
+ * }
+ */
+const usePersistedState = <T extends string | number | object | null | undefined>(
+  key: string,
+  initialValue: T,
+): [T, Dispatch<T>] => {
+  const serializedItem = localStorage.getItem(key);
+  const [state, setState] = useState<T>(serializedItem ? JSON.parse(serializedItem) : initialValue);
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(state));
+  }, [state]);
+
+  return [state, setState];
+};
+
+export default usePersistedState;
