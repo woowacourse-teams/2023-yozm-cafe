769272fa373749ef29afdb3974284ff38b824dac
hum02 <ehdgma02@gmail.com>
2023-08-09 18:14:43 +0900

    refactor:불필요한 쿼리메서드 선언 삭제

--- server/src/main/java/com/project/yozmcafe/domain/cafe/CafeRepository.java
+++ server/src/main/java/com/project/yozmcafe/domain/cafe/CafeRepository.java
@@ -4,12 +4,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface CafeRepository extends JpaRepository<Cafe, Long> {
 
     Slice<Cafe> findSliceBy(Pageable pageable);
-
-    Optional<Cafe> findById(Long id);
 }
 
