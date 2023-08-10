88c347a866320b3d79fcb248757cf4d307111832
solo5star <solo_5@naver.com>
2023-08-11 01:14:28 +0900

    feat: 두 가지 종류의 메뉴 리스트 컴포넌트 구현

---
+++ client/src/components/CafeMenuMiniList.tsx
@@ -0,0 +1,109 @@
+import { PiCoffeeFill } from 'react-icons/pi';
+import { styled } from 'styled-components';
+import type { CafeMenuItem } from '../types';
+
+type CafeMenuMiniListProps = {
+  menus: CafeMenuItem[];
+};
+
+const CafeMenuMiniList = (props: CafeMenuMiniListProps) => {
+  const { menus } = props;
+
+  const recommendedMenus = menus.filter((menuItem) => menuItem.recommended);
+  const otherMenus = menus.filter((menuItem) => !menuItem.recommended);
+
+  return (
+    <MenuList>
+      {recommendedMenus.map((menuItem) => (
+        <MenuListItem key={menuItem.id} menuItem={menuItem} />
+      ))}
+      <Divider />
+      {otherMenus.map((menuItem) => (
+        <MenuListItem key={menuItem.id} menuItem={menuItem} />
+      ))}
+    </MenuList>
+  );
+};
+
+export default CafeMenuMiniList;
+
+const MenuList = styled.ul`
+  overflow-y: auto;
+  display: flex;
+  flex-shrink: 0;
+  gap: ${({ theme }) => theme.space[4]};
+`;
+
+type MenuListItemProps = {
+  menuItem: CafeMenuItem;
+};
+
+const MenuListItem = (props: MenuListItemProps) => {
+  const { menuItem } = props;
+
+  return (
+    <MenuListItemContainer>
+      <MenuItemImageContainer>
+        {menuItem.imageUrl ? (
+          <MenuItemImage src={menuItem.imageUrl} />
+        ) : (
+          <MenuItemImageAlt>
+            <MenuItemImageAltIcon />
+          </MenuItemImageAlt>
+        )}
+      </MenuItemImageContainer>
+      <MenuItemName>{menuItem.name}</MenuItemName>
+    </MenuListItemContainer>
+  );
+};
+
+const MenuListItemContainer = styled.li`
+  display: flex;
+  flex-direction: column;
+  flex-shrink: 0;
+  align-items: center;
+
+  width: 70px;
+  height: 100%;
+`;
+
+const MenuItemImageContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: ${({ theme }) => theme.space[2]};
+
+  & > * {
+    aspect-ratio: 1 / 1;
+    width: 100%;
+  }
+`;
+
+const MenuItemImage = styled.img`
+  display: block;
+  object-fit: cover;
+  border-radius: 50%;
+`;
+
+const MenuItemImageAlt = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  background: #dedede;
+  border-radius: 50%;
+`;
+
+const MenuItemImageAltIcon = styled(PiCoffeeFill)`
+  width: 50%;
+  height: 50%;
+`;
+
+const MenuItemName = styled.div`
+  font-size: ${({ theme }) => theme.fontSize.xs};
+`;
+
+const Divider = styled.hr`
+  margin: ${({ theme }) => theme.space[2]} ${({ theme }) => theme.space[1]};
+  border: none;
+  border-left: 1px solid ${({ theme }) => theme.color.gray};
+`;
