fb9b9e9738663bcf71f9edc4da6f122075927507
jeongwusi <jeongwusi@gmail.com>
2023-08-10 22:46:29 +0900

    feat: 카페 단건 조회를 할 수 있는 경로 설정

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,6 +1,7 @@
 import { Suspense } from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import Auth from './pages/Auth';
+import Cafe from './pages/Cafe';
 import Home from './pages/Home';
 import Loading from './pages/Loading';
 import MyProfile from './pages/MyProfile';
@@ -16,6 +17,7 @@ const router = createBrowserRouter([
     children: [
       { index: true, element: <Home /> },
       { path: 'my-profile', element: <MyProfile /> },
+      { path: '/cafe/:cafeId', element: <Cafe /> },
     ],
   },
   {
