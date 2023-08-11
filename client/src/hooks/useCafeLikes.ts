5a93a093745c796578c35466efbd2a349ddc69c1
jeongwusi <jeongwusi@gmail.com>
2023-08-11 11:52:05 +0900

    refactor: useCafeLikes 커스텀 훅 useState를 이용해 개선

--- client/src/hooks/useCafeLikes.ts
+++ client/src/hooks/useCafeLikes.ts
@@ -1,6 +1,7 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useState } from 'react'; // useState 가져오기
 import client from '../client';
-import { Cafe } from '../types';
+import { type Cafe } from '../types';
 import useAuth from './useAuth';
 
 type SetLikedCafeParams = {
@@ -10,19 +11,20 @@ type SetLikedCafeParams = {
 const useCafeLikes = (cafe: Cafe) => {
   const queryClient = useQueryClient();
   const { identity } = useAuth();
+  const [isLiked, internalSetIsLiked] = useState(cafe.isLiked);
   const { mutate: setLiked } = useMutation({
     mutationFn: ({ isLiked }: SetLikedCafeParams) => client.setLikedCafe(cafe.id, isLiked),
     onMutate: async ({ isLiked }) => {
-      const previous = queryClient.getQueryData(['cafes', cafe.id, 'likes', identity]);
-      queryClient.setQueryData(['cafes', cafe.id, 'likes', identity], isLiked);
+      const previous = isLiked;
+      internalSetIsLiked(isLiked);
       return { previous };
     },
     onError: (_, __, context) => {
-      queryClient.setQueryData(['cafes', cafe.id, 'likes', identity], context?.previous);
+      internalSetIsLiked(context?.previous ?? cafe.isLiked);
+      queryClient.invalidateQueries(['cafes', identity], undefined);
+      queryClient.invalidateQueries(['likedCafeDetail', identity], undefined);
     },
   });
-
-  const isLiked = queryClient.getQueryData<boolean>(['cafes', cafe.id, 'likes', identity]) ?? cafe.isLiked;
 
   return { isLiked, setLiked };
 };
