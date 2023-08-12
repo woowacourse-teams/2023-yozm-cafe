f6440a1d3828f730dfe0b807eb5f3abf8ceac2cd
dev_kong <ahdjd5@gmail.com>
2023-08-12 10:11:37 +0900

    feat: Post /admin/cafes 를 통해 카페를 저장하고 이미지를 업로드하는 api 구현

--- server/src/main/java/com/project/yozmcafe/controller/CafeAdminController.java
+++ server/src/main/java/com/project/yozmcafe/controller/CafeAdminController.java
@@ -1,9 +1,8 @@
 package com.project.yozmcafe.controller;
 
-import com.project.yozmcafe.controller.dto.cafe.CafeRequest;
-import com.project.yozmcafe.controller.dto.cafe.CafeResponse;
-import com.project.yozmcafe.controller.dto.cafe.CafeUpdateRequest;
-import com.project.yozmcafe.service.CafeAdminService;
+import java.net.URI;
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,23 +12,32 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.net.URI;
-import java.util.List;
+import com.project.yozmcafe.controller.dto.cafe.CafeRequest;
+import com.project.yozmcafe.controller.dto.cafe.CafeResponse;
+import com.project.yozmcafe.controller.dto.cafe.CafeUpdateRequest;
+import com.project.yozmcafe.service.CafeAdminService;
+import com.project.yozmcafe.service.ImageService;
 
 @Controller
 @RequestMapping("/admin/cafes")
 public class CafeAdminController {
 
     private final CafeAdminService cafeAdminService;
+    private final ImageService imageService;
 
-    public CafeAdminController(final CafeAdminService cafeAdminService) {
+    public CafeAdminController(final CafeAdminService cafeAdminService, final ImageService imageService) {
         this.cafeAdminService = cafeAdminService;
+        this.imageService = imageService;
     }
 
     @PostMapping
-    public ResponseEntity<String> save(@RequestBody final CafeRequest request) {
-        final Long savedId = cafeAdminService.save(request);
+    public ResponseEntity<String> save(@RequestPart final CafeRequest request,
+                                       @RequestPart final List<MultipartFile> images) {
+        final List<String> uploadedFileNames = imageService.upload(images);
+        final Long savedId = cafeAdminService.save(request, uploadedFileNames);
         return ResponseEntity.created(URI.create("/admin/cafes/" + savedId)).build();
     }
 
--- server/src/test/java/com/project/yozmcafe/controller/CafeAdminControllerTest.java
+++ server/src/test/java/com/project/yozmcafe/controller/CafeAdminControllerTest.java
@@ -1,54 +1,68 @@
 package com.project.yozmcafe.controller;
 
+import static com.epages.restdocs.apispec.RestAssuredRestDocumentationWrapper.document;
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static io.restassured.http.ContentType.MULTIPART;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestPartBody;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+
+import java.io.File;
+import java.time.LocalTime;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.mock.web.MockMultipartFile;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.yozmcafe.controller.dto.cafe.AvailableTimeRequest;
 import com.project.yozmcafe.controller.dto.cafe.CafeRequest;
 import com.project.yozmcafe.controller.dto.cafe.CafeUpdateRequest;
 import com.project.yozmcafe.controller.dto.cafe.DetailRequest;
+import com.project.yozmcafe.domain.S3Client;
 import com.project.yozmcafe.domain.cafe.available.Days;
-import io.restassured.http.ContentType;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpStatus;
-
-import java.time.LocalTime;
-import java.util.List;
-
-import static com.epages.restdocs.apispec.RestAssuredRestDocumentationWrapper.document;
-import static io.restassured.RestAssured.given;
-import static io.restassured.http.ContentType.JSON;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
-import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
-import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
-import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 
 class CafeAdminControllerTest extends BaseControllerTest {
+
+
+    @Autowired
+    private ObjectMapper mapper;
+    @MockBean
+    private S3Client s3Client;
+
+    private File image = new File("src/test/resources/image.png");
+
 
     @Test
     @DisplayName("카페 저장하기")
     void save() {
         //given
-        final CafeRequest request = makeCafeRequest();
+        doNothing().when(s3Client).upload(any(MockMultipartFile.class));
+        final String cafeRequestJson = makeCafeRequest();
 
         //when, then
         given(spec).log().all()
-                .contentType(JSON)
+                .contentType(MULTIPART)
+                .multiPart("request", cafeRequestJson, "application/json")
+                .multiPart("images", image, "image/png")
+                .accept(JSON)
                 .filter(document("어드민 API/카페 저장하기",
-                        requestFields(
-                                fieldWithPath("name").description("카페 이름"),
-                                fieldWithPath("address").description("카페 도로명 주소"),
-                                fieldWithPath("images.[]").description("이미지 URL 배열"),
-                                fieldWithPath("detail.mapUrl").description("네이버 지도 URL"),
-                                fieldWithPath("detail.description").description("상세 설명"),
-                                fieldWithPath("detail.phone").description("전화번호"),
-                                fieldWithPath("detail.openingHours.[].day").description("요일"),
-                                fieldWithPath("detail.openingHours.[].open").description("영업 시작 시각"),
-                                fieldWithPath("detail.openingHours.[].close").description("영업 종료 시각"),
-                                fieldWithPath("detail.openingHours.[].opened").description("해당 요일 영업 여부")),
+                        requestPartBody("request"), requestPartBody("images"),
                         responseHeaders(headerWithName("Location").description("카페 저장 위치"))))
-                .body(request)
                 .when()
                 .post("/admin/cafes")
                 .then()
@@ -169,23 +183,29 @@ class CafeAdminControllerTest extends BaseControllerTest {
     }
 
     private String saveCafe() {
-        CafeRequest request = makeCafeRequest();
-        String location = given()
-                .contentType(ContentType.JSON)
-                .body(request)
+        final String cafeRequest = makeCafeRequest();
+
+        final String location = given().log().all()
+                .contentType(MULTIPART)
+                .multiPart("request", cafeRequest, "application/json")
+                .multiPart("images", image, "image/png")
+                .accept(JSON)
                 .when()
                 .post("/admin/cafes")
                 .then()
                 .extract().header("Location");
+
         String[] split = location.split("/");
 
         return split[split.length - 1];
     }
 
-    private CafeRequest makeCafeRequest() {
-        return new CafeRequest("연어카페", "광안리",
-                List.of("https://image5jvqbd.fmkorea.com/files/attach/new2/20221010/486616/3539993806/5096425812/9ccc34aeb4ea8d131d7f9b66919dfdec.jpg"),
-                detail());
+    private String makeCafeRequest() {
+        try {
+            return mapper.writeValueAsString(new CafeRequest("연어카페", "광안리", detail()));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private CafeUpdateRequest makeCafeUpdateRequest() {
@@ -196,13 +216,20 @@ class CafeAdminControllerTest extends BaseControllerTest {
     }
 
     private DetailRequest detail() {
-        final AvailableTimeRequest time1 = new AvailableTimeRequest(Days.MONDAY, LocalTime.now(), LocalTime.now(), true);
-        final AvailableTimeRequest time2 = new AvailableTimeRequest(Days.TUESDAY, LocalTime.now(), LocalTime.now(), true);
-        final AvailableTimeRequest time3 = new AvailableTimeRequest(Days.WEDNESDAY, LocalTime.now(), LocalTime.now(), true);
-        final AvailableTimeRequest time4 = new AvailableTimeRequest(Days.THURSDAY, LocalTime.now(), LocalTime.now(), true);
-        final AvailableTimeRequest time5 = new AvailableTimeRequest(Days.FRIDAY, LocalTime.now(), LocalTime.now(), true);
-        final AvailableTimeRequest time6 = new AvailableTimeRequest(Days.SATURDAY, LocalTime.now(), LocalTime.now(), true);
-        final AvailableTimeRequest time7 = new AvailableTimeRequest(Days.SUNDAY, LocalTime.now(), LocalTime.now(), true);
+        final AvailableTimeRequest time1 = new AvailableTimeRequest(Days.MONDAY, LocalTime.now(), LocalTime.now(),
+                true);
+        final AvailableTimeRequest time2 = new AvailableTimeRequest(Days.TUESDAY, LocalTime.now(), LocalTime.now(),
+                true);
+        final AvailableTimeRequest time3 = new AvailableTimeRequest(Days.WEDNESDAY, LocalTime.now(), LocalTime.now(),
+                true);
+        final AvailableTimeRequest time4 = new AvailableTimeRequest(Days.THURSDAY, LocalTime.now(), LocalTime.now(),
+                true);
+        final AvailableTimeRequest time5 = new AvailableTimeRequest(Days.FRIDAY, LocalTime.now(), LocalTime.now(),
+                true);
+        final AvailableTimeRequest time6 = new AvailableTimeRequest(Days.SATURDAY, LocalTime.now(), LocalTime.now(),
+                true);
+        final AvailableTimeRequest time7 = new AvailableTimeRequest(Days.SUNDAY, LocalTime.now(), LocalTime.now(),
+                true);
 
         return new DetailRequest(List.of(time1, time2, time3, time4, time5, time6, time7),
                 "지도 url", "존맛탱구리", "01032472601");
