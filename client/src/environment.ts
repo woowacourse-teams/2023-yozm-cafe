206b9a3f9c8cfa202993f3ddeee92d5e318fd76e
jeongwusi <jeongwusi@gmail.com>
2023-08-16 17:33:09 +0900

    feat: environment 파일에 사용자의 환경 설정을 확인할 수 있는 로직 추가

--- client/src/environment.ts
+++ client/src/environment.ts
@@ -1,3 +1,5 @@
 export const IS_DEV = process.env.NODE_ENV === 'development';
 
 export const MSW = (process.env.MSW ?? (IS_DEV ? 'true' : 'false'))?.toLowerCase() === 'true';
+
+export const ImageEnv = process.env.NODE_ENV === 'development' ? 'dev' : 'prod';
