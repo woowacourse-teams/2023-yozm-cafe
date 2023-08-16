3fefa719bfa0ea5f974dbab2320ac7fe5dcf3dbf
jeongwusi <jeongwusi@gmail.com>
2023-08-16 19:48:40 +0900

    refactor: environment 파일에 사용자의 환경 설정을 확인할 수 있는 로직 수정

--- client/src/environment.ts
+++ client/src/environment.ts
@@ -2,4 +2,4 @@ export const IS_DEV = process.env.NODE_ENV === 'development';
 
 export const MSW = (process.env.MSW ?? (IS_DEV ? 'true' : 'false'))?.toLowerCase() === 'true';
 
-export const ImageEnv = process.env.NODE_ENV === 'development' ? 'dev' : 'prod';
+export const IMAGE_HOST = IS_DEV ? 'https://image.yozm.cafe/dev' : 'https://image.yozm.cafe/prod';
