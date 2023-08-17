7752e8d3f5ced3d57639ac06c3664fc0fd10c32b
solo5star <solo_5@naver.com>
2023-08-17 21:30:49 +0900

    fix: 잘못된 path join 로직 수정

--- client/src/utils/Image.ts
+++ client/src/utils/Image.ts
@@ -3,7 +3,12 @@ import { IMAGE_HOST } from '../environment';
 class Image {
   static getUrl(options: { filename: string; size: 100 | 500 }) {
     const { filename, size } = options;
-    return IMAGE_HOST({ size }) + filename;
+    const paths = [IMAGE_HOST({ size }), filename];
+
+    return paths
+      .filter((path) => path)
+      .join('/')
+      .replace(/(?<!:)\/{2,}/, '/');
   }
 }
 
