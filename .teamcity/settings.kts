346f965a5b788a835abbcd07e17a8dd184c42f75
solo5star <solo_5@naver.com>
2023-08-21 05:02:49 +0900

    build: settings.kts 재작성

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,30 +1,78 @@
-import jetbrains.buildServer.configs.kotlin.v2018_2.*
-import jetbrains.buildServer.configs.kotlin.v2018_2.buildFeatures.perfmon
-import jetbrains.buildServer.configs.kotlin.v2018_2.buildFeatures.sshAgent
-import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.SSHUpload
-import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.nodeJS
-import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.sshUpload
-import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.dockerCompose
-import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.gradle
-import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.script
-import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.sshExec
-import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.sshUpload
-import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.vcs
-import jetbrains.buildServer.configs.kotlin.v2018_2.vcs.GitVcsRoot
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
+import jetbrains.buildServer.configs.kotlin.buildFeatures.sshAgent
+import jetbrains.buildServer.configs.kotlin.buildSteps.SSHUpload
+import jetbrains.buildServer.configs.kotlin.buildSteps.nodeJS
+import jetbrains.buildServer.configs.kotlin.buildSteps.sshUpload
+import jetbrains.buildServer.configs.kotlin.buildSteps.dockerCompose
+import jetbrains.buildServer.configs.kotlin.buildSteps.gradle
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.buildSteps.sshExec
+import jetbrains.buildServer.configs.kotlin.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
 
-version = "2018.2"
+version = "2023.05"
+
+// TeamCity에 프로젝트를 추가할 때 입력해야 하는 초기 값들입니다
+val repository = DslContext.getParameter("repository")
+
+// 배포 대상 서버의 정보(host, port, username)을 입력합니다.
+val deployTargetProdHost = DslContext.getParameter("deploy_target.prod.host")
+val deployTargetProdPort = DslContext.getParameter("deploy_target.prod.port")
+val deployTargetProdUsername = DslContext.getParameter("deploy_target.prod.username")
+val deployTargetDevHost = DslContext.getParameter("deploy_target.dev.host")
+val deployTargetDevPort = DslContext.getParameter("deploy_target.dev.port")
+val deployTargetDevUsername = DslContext.getParameter("deploy_target.dev.username")
 
 project {
-    buildType(Server)
-    buildType(Client)
+    description = "yozm.cafe 프로젝트의 CI/CD 파이프라인 스크립트입니다"
+
+    val gitHubProd = GitVcsRoot {
+        id = RelativeId("ProdVcs")
+        name = "2023-yozm-cafe prod"
+        url = repository
+        branch = "main"
+        branchSpec = "+:refs/heads/main"
+    }
+
+    val gitHubDev = GitVcsRoot {
+        id = RelativeId("DevVcs")
+        name = "2023-yozm-cafe dev"
+        url = repository
+        branch = "dev"
+        branchSpec = "+:refs/heads/dev"
+    }
+
+    vcsRoot(gitHubProd)
+    vcsRoot(gitHubDev)
+
+    // 4개의 빌드 설정을 추가합니다
+    buildType(ServerBuildType("prod", gitHubProd, deployTargetProdHost, deployTargetProdPort, deployTargetProdUsername))
+    buildType(ServerBuildType("dev", gitHubDev, deployTargetDevHost, deployTargetDevPort, deployTargetDevUsername))
+    buildType(ClientBuildType("prod", gitHubProd, deployTargetProdHost, deployTargetProdPort, deployTargetProdUsername))
+    buildType(ClientBuildType("dev", gitHubDev, deployTargetDevHost, deployTargetDevPort, deployTargetDevUsername))
 }
 
-object Server : BuildType({
-    name = "Server"
+open class ServerBuildType(
+        private val buildMode: String,
+        private val vcsRoot: VcsRoot,
+        private val deployTargetHost: String,
+        private val deployTargetPort: String,
+        private val deployTargetUsername: String,
+) : BuildType({
+    id("YozmCafe_Server_$buildMode")
+    name = "Server:$buildMode"
+    description = "서버 CI/CD"
+
+    vcs {
+        root(vcsRoot)
+        cleanCheckout = true
+    }
 
     params {
-        param("env.APPLICATION_DEV", """
-        """.trimIndent())
+        param("DEPLOY_TARGET_HOST", deployTargetHost)
+        param("DEPLOY_TARGET_PORT", deployTargetPort)
+        param("DEPLOY_TARGET_USERNAME", deployTargetUsername)
     }
 
     steps {
@@ -41,16 +89,17 @@ object Server : BuildType({
             name = "빌드"
             tasks = "clean build"
             buildFile = "server/build.gradle"
-            gradleParams = "-Pprofile=dev"
+            gradleParams = "-Pprofile=$buildMode"
             gradleWrapperPath = "server"
         }
         sshUpload {
             name = "빌드 파일 업로드"
             transportProtocol = SSHUpload.TransportProtocol.SCP
             sourcePath = "server/build/libs/yozm-cafe-0.0.1-SNAPSHOT.jar"
-            targetUrl = "ec2-43-201-57-25.ap-northeast-2.compute.amazonaws.com"
+            targetUrl = "%DEPLOY_TARGET_HOST%"
+            param("jetbrains.buildServer.sshexec.port", "%DEPLOY_TARGET_PORT%")
             authMethod = sshAgent {
-                username = "ubuntu"
+                username = "%DEPLOY_TARGET_USERNAME%"
             }
         }
         sshExec {
@@ -60,12 +109,13 @@ object Server : BuildType({
                     echo "Port 8080 is already in use. Killing the process..."
                     sudo lsof -i :8080 | awk 'NR!=1 {print ${'$'}2}' | sudo xargs kill -9
                 fi
-                
+
                 nohup java -jar yozm-cafe-0.0.1-SNAPSHOT.jar > nohup.out 2> nohup.err < /dev/null &
             """.trimIndent()
-            targetUrl = "ec2-43-201-57-25.ap-northeast-2.compute.amazonaws.com"
+            targetUrl = "%DEPLOY_TARGET_HOST%"
+            param("jetbrains.buildServer.sshexec.port", "%DEPLOY_TARGET_PORT%")
             authMethod = sshAgent {
-                username = "ubuntu"
+                username = "%DEPLOY_TARGET_USERNAME%"
             }
         }
     }
@@ -73,9 +123,9 @@ object Server : BuildType({
     triggers {
         vcs {
             triggerRules = "+:/server"
-            branchFilter = "+:main"
         }
     }
+
     features {
         perfmon {
         }
@@ -85,8 +135,27 @@ object Server : BuildType({
     }
 })
 
-object Client : BuildType({
-    name = "Client"
+open class ClientBuildType(
+        private val buildMode: String,
+        private val vcsRoot: VcsRoot,
+        private val deployTargetHost: String,
+        private val deployTargetPort: String,
+        private val deployTargetUsername: String,
+) : BuildType({
+    id("YozmCafe_Client_$buildMode")
+    name = "Client:$buildMode"
+    description = "클라이언트 CI/CD"
+
+    vcs {
+        root(vcsRoot)
+        cleanCheckout = true
+    }
+
+    params {
+        param("DEPLOY_TARGET_HOST", deployTargetHost)
+        param("DEPLOY_TARGET_PORT", deployTargetPort)
+        param("DEPLOY_TARGET_USERNAME", deployTargetUsername)
+    }
 
     steps {
         nodeJS {
@@ -101,9 +170,10 @@ object Client : BuildType({
             name = "Deploy build files"
             transportProtocol = SSHUpload.TransportProtocol.SCP
             sourcePath = "client/dist/**"
-            targetUrl = "ec2-43-201-57-25.ap-northeast-2.compute.amazonaws.com:public"
+            targetUrl = "%DEPLOY_TARGET_HOST%:public"
+            param("jetbrains.buildServer.sshexec.port", "%DEPLOY_TARGET_PORT%")
             authMethod = sshAgent {
-                username = "ubuntu"
+                username = "%DEPLOY_TARGET_USERNAME%"
             }
         }
     }
@@ -111,7 +181,6 @@ object Client : BuildType({
     triggers {
         vcs {
             triggerRules = "+:/client"
-            branchFilter = "+:main"
         }
     }
 
