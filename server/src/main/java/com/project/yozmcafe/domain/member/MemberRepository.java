4d13d8c7c45dd63fbed365bb5d3c05ea7b2acbab
donghae-kim <donghae1999@gmail.com>
2023-09-12 23:43:41 +0900

    refactor: @Param 추가

--- server/src/main/java/com/project/yozmcafe/domain/member/MemberRepository.java
+++ server/src/main/java/com/project/yozmcafe/domain/member/MemberRepository.java
@@ -2,6 +2,7 @@ package com.project.yozmcafe.domain.member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,19 +11,21 @@ import java.util.Optional;
 @Repository
 public interface MemberRepository extends JpaRepository<Member, String> {
 
+    @Override
     @Query("""
-            select m 
-            from Member m 
-            left join fetch m.unViewedCafes uvc 
-            left join fetch uvc.cafe 
+            select m
+            from Member m
+            left join fetch m.unViewedCafes uvc
+            left join fetch uvc.cafe
             where m.id = :id
             """)
-    Optional<Member> findById(String id);
+    Optional<Member> findById(@Param("id") String id);
 
+    @Override
     @Query("""
-            select m 
-            from Member m 
-            left join fetch m.unViewedCafes uvc 
+            select m
+            from Member m
+            left join fetch m.unViewedCafes uvc
             left join fetch uvc.cafe
             """)
     List<Member> findAll();
