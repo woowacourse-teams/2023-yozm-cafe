d72d17b9b8b99e02519ac40f4105dd3f28b99e9c
dev_kong <ahdjd5@gmail.com>
2023-09-15 14:24:27 +0900

    chore: coordinate column에 SRID 값 추가

--- server/src/main/resources/db/migration/V20230914151731__add_point_column.sql
+++ server/src/main/resources/db/migration/V20230914151731__add_point_column.sql
@@ -1,7 +1,7 @@
 CREATE TABLE cafe_coordinates
 (
     id         BIGINT NOT NULL AUTO_INCREMENT,
-    coordinate POINT  NOT NULL,
+    coordinate POINT  NOT NULL SRID 4326,
     cafe_id    BIGINT NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (cafe_id) REFERENCES cafe (id)
