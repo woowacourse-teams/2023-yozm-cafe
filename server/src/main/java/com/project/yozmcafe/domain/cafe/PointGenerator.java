aae9b1573334d60964b23f5a9d662113dcc0436e
dev_kong <ahdjd5@gmail.com>
2023-09-20 21:04:36 +0900

    chore: SRID 설명 주석 추가

--- server/src/main/java/com/project/yozmcafe/domain/cafe/PointGenerator.java
+++ server/src/main/java/com/project/yozmcafe/domain/cafe/PointGenerator.java
@@ -7,6 +7,11 @@ import org.locationtech.jts.geom.Point;
 public class PointGenerator {
 
     private static final GeometryFactory GEOMETRY_FACTORY = new GeometryFactory();
+    /**
+     * SRID(Spatial Reference Identifier)는 고유한 공간 좌표 식별자입니다.
+     * <p>
+     * 그 중, 4326은 WGS84 경위도 좌표계를 의미합니다. GPS 기술에서도 사용되는 좌표계로 범용적으로 가장 널리 사용되는 좌표계입니다.
+     */
     private static final int SRID = 4326;
     private static final String STRING_POINT_FORMAT = "POINT(%s)";
     private static final String STRING_POINT_DELIMITER = " ";
