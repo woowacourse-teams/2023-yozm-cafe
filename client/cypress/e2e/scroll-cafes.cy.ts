3e5db89f937b9ef432d3a686b440eb994d561046
AhnGeunTaek <dksrmsxor@gmail.com>
2023-09-20 10:49:30 +0900

    feat: 스크롤 시 다음 카페 이미지가 나오는지에 대한 cypress test 작성

--- client/cypress/e2e/scroll-cafes.cy.ts
+++ client/cypress/e2e/scroll-cafes.cy.ts
@@ -13,4 +13,9 @@ context('Scroll Cafes', () => {
     cy.get('main > ul').scrollTo(0, 400);
     cy.contains('성수 카페2 (1호점)').should('be.visible');
   });
+
+  it('스크롤을 2번 하면 세 번째 카페가 나타나야 한다.', () => {
+    cy.get('main > ul').scrollTo(0, 400 * 3);
+    cy.contains('성수 카페3 (1호점)').should('be.visible');
+  });
 });
