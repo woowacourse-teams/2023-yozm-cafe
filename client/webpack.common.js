01e19975ceb348518bc15563d0b75f11d98aa224
solo5star <solo_5@naver.com>
2023-09-28 12:59:48 +0900

    feat: favicons-webpack-plugin 적용
    * web manifest generation 적용
    * apple 계열 홈 화면에 추가 generation 적용
    * apple 휴대폰 splash 이미지 (apple-touch-startup-image) generation 적용
    * favicon.ico는 기존의 것을 사용

--- client/webpack.common.js
+++ client/webpack.common.js
@@ -1,5 +1,6 @@
 import CopyPlugin from 'copy-webpack-plugin';
 import DotenvWebpackPlugin from 'dotenv-webpack';
+import FaviconsWebpackPlugin from 'favicons-webpack-plugin';
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import { dirname } from 'path';
@@ -53,6 +54,23 @@ export default {
     new HtmlWebpackPlugin({
       template: 'index.html',
       filename: 'index.html',
+    }),
+    new FaviconsWebpackPlugin({
+      logo: './src/assets/icon.svg',
+      devMode: 'webapp',
+      persistentCache: true,
+      favicons: {
+        icons: {
+          favicons: false,
+          yandex: false,
+        },
+        appName: '요즘카페',
+        appDescription: '트렌디한 성수 지역의 카페를 손쉽게 탐색하는 서비스, 요즘카페',
+        appleStatusBarStyle: '#ffd3d8',
+        lang: 'ko-KR',
+        background: '#ffffff',
+        theme_color: '#ffd3d8',
+      },
     }),
     new CopyPlugin({
       patterns: [{ from: 'public', to: '' }],
