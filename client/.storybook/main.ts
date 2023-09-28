667d99c946dd2c242303b9e10f808e6b3b6b9adb
solo5star <solo_5@naver.com>
2023-09-28 15:35:44 +0900

    test: storybook fastRefresh 활성화

--- client/.storybook/main.ts
+++ client/.storybook/main.ts
@@ -5,7 +5,9 @@ const config: StorybookConfig = {
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
   framework: {
     name: '@storybook/react-webpack5',
-    options: {},
+    options: {
+      fastRefresh: true,
+    },
   },
   docs: {
     autodocs: 'tag',
