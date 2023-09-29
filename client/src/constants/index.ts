ac0345b26fc73cc16baa4d1c1a08deff19188664
jeongwusi <jeongwusi@gmail.com>
2023-09-29 13:29:11 +0900

    feat: 처음 제공되는 위치와 줌 사이즈 상수화

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -1,6 +1,6 @@
 export const INITIAL_CENTER = {
-  lat: 37.5446,
-  lng: 127.056,
-} as const;
+  lat: 37.543327,
+  lng: 127.056738,
+};
 
-export const INITIAL_ZOOM_SIZE = 16;
+export const INITIAL_ZOOM_SIZE = 17;
