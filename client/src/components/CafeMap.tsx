ec692a130c496e6ed0e49da2a647c2cc62113df1
jeongwusi <jeongwusi@gmail.com>
2023-09-30 14:03:25 +0900

    refactor: CafeMap 컴포넌트 로직 수정

--- client/src/components/CafeMap.tsx
+++ client/src/components/CafeMap.tsx
@@ -1,116 +1,109 @@
-import { useQueryClient } from '@tanstack/react-query';
-import { useEffect, useRef, useState } from 'react';
+import React, { Suspense, useEffect, useRef, useState } from 'react';
 import { styled } from 'styled-components';
-import { SUNGSU_CAFE_STREET_LOCATION, SUNGSU_MAP_INITIAL_ZOOM_SIZE } from '../constants';
-import { useCurrentPosition } from '../hooks/useCurrentPosition';
+import { SEONGSU_CAFE_STREET_LOCATION, SEONGSU_MAP_INITIAL_ZOOM_SIZE } from '../constants';
+import useCafesNearLocation from '../hooks/useCafesNearLocation';
+import useCurrentPosition from '../hooks/useCurrentPosition';
 import CafeMarkersContainer from './CafeMarkersContainer';
 import UserMarker from './UserMarker';
 
-type CafeMapProps = {
-  map: google.maps.Map;
-};
-
-const CafeMapListener = (props: CafeMapProps) => {
-  const { map } = props;
-  const queryClient = useQueryClient();
-
-  useEffect(() => {
-    map.addListener('dragend', () => {
-      queryClient.invalidateQueries({ queryKey: ['cafesNearLocation'] });
-    });
-
-    map.addListener('zoom_changed', () => {
-      queryClient.invalidateQueries({ queryKey: ['cafesNearLocation'] });
-    });
-
-    const initMarkersEvent = map.addListener('bounds_changed', () => {
-      queryClient.invalidateQueries({ queryKey: ['cafesNearLocation'] });
-      google.maps.event.removeListener(initMarkersEvent);
-    });
-  }, [map, queryClient]);
-
-  return <></>;
-};
-
 const CafeMap = () => {
-  const ref = useRef<HTMLDivElement | null>(null);
-  const [googleMap, setGoogleMap] = useState<google.maps.Map>();
-  const [position, setPosition] = useState(SUNGSU_CAFE_STREET_LOCATION);
-  const currentPosition = useCurrentPosition();
-
-  const isClientReady = currentPosition !== undefined && googleMap !== undefined;
-
-  const moveToCurrentUserLocation = () => {
-    if (googleMap && currentPosition) {
-      const { lat, lng } = currentPosition;
-      const bounds = new google.maps.LatLngBounds(
-        new google.maps.LatLng(37.5353, 127.0299),
-        new google.maps.LatLng(37.5543, 127.0637),
-      );
-
-      // 원하는 위치가 범위 내에 있는지 확인합니다.
-      if (bounds.contains(new google.maps.LatLng(lat, lng))) {
-        googleMap.panTo({ lat, lng });
-        setPosition({ lat, lng });
-      } else {
-        // 경고 메시지를 표시합니다.
-        alert('서비스는 현재 성수 지역에서만 이용 가능합니다.');
-        googleMap.panTo(SUNGSU_CAFE_STREET_LOCATION);
-        setPosition(SUNGSU_CAFE_STREET_LOCATION);
-      }
-    }
-  };
-
-  const moveToSungsuCafeRoadLocation = () => {
-    if (googleMap) {
-      googleMap.panTo(SUNGSU_CAFE_STREET_LOCATION);
-      setPosition(SUNGSU_CAFE_STREET_LOCATION);
-    }
-  };
+  const ref = useRef<HTMLDivElement>(null);
+  const [googleMap, setGoogleMap] = useState<google.maps.Map | null>(null);
 
   useEffect(() => {
-    if (ref.current) {
-      const initialMap = new window.google.maps.Map(ref.current, {
-        center: position,
-        zoom: SUNGSU_MAP_INITIAL_ZOOM_SIZE,
-        disableDefaultUI: true,
-        clickableIcons: false,
-        mapId: '32c9cce63f7772a8',
-        maxZoom: 20,
-        minZoom: 14,
-        restriction: {
-          latLngBounds: {
-            north: 37.5543,
-            south: 37.5353,
-            east: 127.0637,
-            west: 127.0299,
-          },
-        },
-      });
+    if (!ref.current) return;
 
-      setGoogleMap(initialMap);
-    }
-  }, [position]);
+    const googleMap = new window.google.maps.Map(ref.current, {
+      center: SEONGSU_CAFE_STREET_LOCATION,
+      zoom: SEONGSU_MAP_INITIAL_ZOOM_SIZE,
+      disableDefaultUI: true,
+      clickableIcons: false,
+      mapId: '32c9cce63f7772a8',
+      maxZoom: 20,
+      minZoom: 14,
+      restriction: {
+        latLngBounds: {
+          north: 37.5543,
+          south: 37.5353,
+          east: 127.0637,
+          west: 127.0299,
+        },
+      },
+    });
+
+    setGoogleMap(googleMap);
+  }, []);
 
   return (
     <>
       <div ref={ref} id="map" style={{ minHeight: '100vh' }} />
-      <MapLocationButtonContainer>
-        <MapLocationButton onClick={moveToCurrentUserLocation}>내 위치로 이동</MapLocationButton>
-        <MapLocationButton onClick={moveToSungsuCafeRoadLocation}>성수동 카페거리로 이동</MapLocationButton>
-      </MapLocationButtonContainer>
-      {isClientReady && (
-        <>
-          <CafeMarkersContainer map={googleMap} />
-          <UserMarker map={googleMap} currentPosition={currentPosition} />
-          <CafeMapListener map={googleMap} />
-        </>
-      )}
+      <Suspense>{googleMap && <CafeMapContent map={googleMap} />}</Suspense>
     </>
   );
 };
 
-export default CafeMap;
+type CafeMapContentProps = {
+  map: google.maps.Map;
+};
+
+const CafeMapContent = (props: CafeMapContentProps) => {
+  const { map } = props;
+
+  const currentPosition = useCurrentPosition();
+  const { refetch } = useCafesNearLocation(map);
+
+  const setPosition = (position: { lat: number; lng: number }) => {
+    map.setCenter(position);
+  };
+
+  const moveToCurrentUserLocation = () => {
+    if (!currentPosition) return;
+
+    const { lat, lng } = currentPosition;
+    const bounds = new google.maps.LatLngBounds(
+      new google.maps.LatLng(37.5353, 127.0299),
+      new google.maps.LatLng(37.5543, 127.0637),
+    );
+
+    // 원하는 위치가 범위 내에 있는지 확인합니다.
+    if (bounds.contains(new google.maps.LatLng(lat, lng))) {
+      map.panTo({ lat, lng });
+      setPosition({ lat, lng });
+    } else {
+      // 경고 메시지를 표시합니다.
+      alert('서비스는 성수 지역에서만 이용 가능합니다.');
+      map.panTo(SEONGSU_CAFE_STREET_LOCATION);
+      setPosition(SEONGSU_CAFE_STREET_LOCATION);
+    }
+  };
+
+  const moveToSungsuCafeRoadLocation = () => {
+    map.panTo(SEONGSU_CAFE_STREET_LOCATION);
+    setPosition(SEONGSU_CAFE_STREET_LOCATION);
+  };
+
+  useEffect(() => {
+    const listener = map.addListener('idle', () => {
+      refetch();
+    });
+
+    return () => google.maps.event.removeListener(listener);
+  }, [map]);
+
+  return (
+    <>
+      <MapLocationButtonContainer>
+        <MapLocationButton onClick={moveToCurrentUserLocation}>내 위치로 이동</MapLocationButton>
+        <MapLocationButton onClick={moveToSungsuCafeRoadLocation}>성수동 카페거리로 이동</MapLocationButton>
+      </MapLocationButtonContainer>
+
+      <CafeMarkersContainer map={map} />
+      {currentPosition && <UserMarker map={map} currentPosition={currentPosition} />}
+    </>
+  );
+};
+
+export default React.memo(CafeMap);
 
 const MapLocationButtonContainer = styled.div`
   position: absolute;
