8f8bd53a2b378fc3ed8938806d1c909be8bf9e6a
solo5star <solo_5@naver.com>
2023-10-08 17:35:51 +0900

    feat: ErrorBoundary, QueryErrorBoundary 몇몇 props 추가
    * caught, FallbackComponent 추가
    * fallbackRender에서 error도 주입

--- client/src/components/QueryErrorBoundary.tsx
+++ client/src/components/QueryErrorBoundary.tsx
@@ -1,20 +1,28 @@
 import { QueryErrorResetBoundary, onlineManager } from '@tanstack/react-query';
-import type { PropsWithChildren } from 'react';
 import { useEffect, useState } from 'react';
 import type { ErrorBoundaryProps } from './ErrorBoundary';
 import ErrorBoundary from './ErrorBoundary';
+import ErrorRetryPrompt from './ErrorRetryPrompt';
 
-type QueryErrorBoundaryProps = PropsWithChildren<ErrorBoundaryProps>;
+type QueryErrorBoundaryProps<TError> = ErrorBoundaryProps<TError>;
 
-const QueryErrorBoundary = (props: QueryErrorBoundaryProps) => {
+/**
+ * react-query에서 `useQuery` 혹은 `useMutation` 등의 쿼리 함수를
+ * `suspense: true`로 사용 중 발생하는 에러를 위한
+ * ErrorBoundary 컴포넌트입니다.
+ *
+ * ErrorBoundary에서 다음 부가 기능이 추가되어있습니다
+ * * network 상태가 온라인으로 전환되었을 때 자동으로 재시도(error reset)
+ * * ErrorBoundary reset 시, query cache도 reset
+ * * 아무런 fallback이 주어지지 않았을 시 {@link ErrorRetryPrompt} 를 기본으로 사용
+ */
+const QueryErrorBoundary = <TError = Error,>(props: QueryErrorBoundaryProps<TError>) => {
   const { children, onReset, ...restProps } = props;
   const [id, setId] = useState(0);
 
   useEffect(() => {
     return onlineManager.subscribe(() => {
-      if (onlineManager.isOnline()) {
-        setId((id) => id + 1);
-      }
+      if (onlineManager.isOnline()) setId((id) => id + 1);
     });
   }, []);
 
@@ -27,7 +35,9 @@ const QueryErrorBoundary = (props: QueryErrorBoundaryProps) => {
             reset();
             onReset?.();
           }}
-          {...restProps}
+          {...('FallbackComponent' in restProps || 'fallbackRender' in restProps
+            ? restProps
+            : { FallbackComponent: ErrorRetryPrompt, ...restProps })}
         >
           {children}
         </ErrorBoundary>
