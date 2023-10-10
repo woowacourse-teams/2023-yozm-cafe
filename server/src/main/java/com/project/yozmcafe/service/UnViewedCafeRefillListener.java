62f0691c87e99e6e15ad0083ee34345eae5ed134
hum02 <ehdgma02@gmail.com>
2023-10-10 17:59:43 +0900

    refactor(UnViewedCafeRefillListener):예외발생 전파 막기 위해 try-catch

--- server/src/main/java/com/project/yozmcafe/service/UnViewedCafeRefillListener.java
+++ server/src/main/java/com/project/yozmcafe/service/UnViewedCafeRefillListener.java
@@ -1,6 +1,8 @@
 package com.project.yozmcafe.service;
 
 import com.project.yozmcafe.domain.member.Member;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,6 +11,8 @@ import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 public class UnViewedCafeRefillListener {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UnViewedCafeRefillListener.class);
 
     private final UnViewedCafeService unViewedCafeService;
 
@@ -20,6 +24,10 @@ public class UnViewedCafeRefillListener {
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void listen(final CafeRefillEvent event) {
         final Member member = event.getMember();
-        unViewedCafeService.refillUnViewedCafes(member);
+        try {
+            unViewedCafeService.refillUnViewedCafes(member);
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage(), e);
+        }
     }
 }
