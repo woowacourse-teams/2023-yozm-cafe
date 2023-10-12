81c8c39511aab4c7e89eeca70d7b8d4fa68cecae
jeongwusi <jeongwusi@gmail.com>
2023-10-12 17:19:41 +0900

    refactor: 로딩 중 UI를 로딩바 컴포넌트로 수정

--- client/src/pages/CafeMapPage.tsx
+++ client/src/pages/CafeMapPage.tsx
@@ -1,13 +1,19 @@
 import { Status, Wrapper } from '@googlemaps/react-wrapper';
 import { type ReactElement } from 'react';
+import styled from 'styled-components';
 import CafeMap from '../components/CafeMap';
+import LoadingBar from '../components/LoadingBar';
 import AppError from '../errors/AppError';
 
 const render = (status: Status): ReactElement => {
   if (status === Status.FAILURE) {
     throw new AppError('지도를 불러오는데 실패하였습니다. 잠시 후 다시 시도해주세요');
   }
-  return <div>로딩중...</div>;
+  return (
+    <Container>
+      <LoadingBar />
+    </Container>
+  );
 };
 
 const CafeMapPage = () => {
@@ -19,3 +25,12 @@ const CafeMapPage = () => {
 };
 
 export default CafeMapPage;
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  height: 100%;
+`;
